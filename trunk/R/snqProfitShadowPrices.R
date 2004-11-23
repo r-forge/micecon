82e46beda123f1440c775cac165f894b1fbad7b4
suapm095 <suapm095@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2004-11-23 18:10:19 +0000

    snqProfitShadowPrices.R many substantial changes - not finished
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@29 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/snqProfitShadowPrices.R
+++ trunk/R/snqProfitShadowPrices.R
@@ -1,24 +1,32 @@
-## ===== calculation of elasticities from beta matrix ===
-snqProfitShadowPrices <- function( coef, price, fix, normPrices ) {
-   if( is.null( dim( price ) ) ) {
-      price <- array( price, c( 1, length( price ) ) )
-   }
-   if( is.null( dim( fix ) ) ) {
-      fix <- array( fix, c( 1, length( fix ) ) )
-   }
-   nNetput <- nrow( coef$delta )
-   nFix    <- ncol( coef$delta )
-   nObs    <- nrow( fix )
-   shadowPrices <- array( 0, c( nObs, nFix ) )
+snqProfitShadowPrices <- function( pNames, fNames, data, weights, coef, form = 0 ) {
 
+   checkNames( c( pNames, fNames ), names( data ) )
+
+   nNetput <- length( pNames )
+   nFix    <- length( fNames )
+   nObs    <- nrow( data )
+
+   snqProfitTestCoef( nNetput, nFix, coef, form = form,
+      coefNames = c( "delta", "gamma" ) )
+
+   normPrice <- numeric( nObs )
+   for( i in 1:nNetput ) {
+      normPrice <- normPrice + data[[ pNames[ i ] ]] * weights[ i ]
+   }
+
+   shadowPrices <- array( 0, c( nObs, nFix ) )
    for( i in 1:nFix ) {
       for( j in 1:nNetput ) {
-         shadowPrices[ , i ] <- shadowPrices[ , i ] + coef$delta[ j, i ] * price[ , j ]
+         shadowPrices[ , i ] <- shadowPrices[ , i ] + coef$delta[ j, i ] *
+            data[[ pNames[ j ] ]]
       }
       for( j in 1:nFix ) {
-         shadowPrices[ , i ] <- shadowPrices[ , i ] + normPrices *
-            coef$gamma[ j, i ] * fix[ , j ]
+         shadowPrices[ , i ] <- shadowPrices[ , i ] + normPrice *
+            coef$gamma[ j, i ] * data[[ fNames[ j ] ]]
       }
    }
-   return( shadowPrices )
+   result <- as.data.frame( shadowPrices )
+   names( result ) <- fNames
+
+   return( result )
 }
