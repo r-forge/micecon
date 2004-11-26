95711d1bd04f1e08a427c0cf52bff6738700b081
suapm095 <suapm095@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2004-11-26 17:48:28 +0000

    aidsEla: changed arguments alpha, beta and gamma to coef
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@63 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/aidsEla.R
+++ trunk/R/aidsEla.R
@@ -1,15 +1,15 @@
-aidsEla <- function( alpha, beta, gamma, W, P = NULL, formula = "AIDS" ) {
+aidsEla <- function( coef, W, P = NULL, formula = "AIDS" ) {
 
-   if( length( alpha ) != length( beta ) ) {
+   if( length( coef$alpha ) != length( coef$beta ) ) {
       stop( "Arguments 'alpha' and 'beta' must have the same length." )
-   } else if( nrow( gamma ) != ncol( gamma ) ) {
+   } else if( nrow( coef$gamma ) != ncol( coef$gamma ) ) {
       stop( "Argument 'gamma' must be a square matrix" )
-   } else if( length( alpha ) != nrow( gamma ) ) {
+   } else if( length( coef$alpha ) != nrow( coef$gamma ) ) {
       stop( paste( "Number of rows of argument 'gamma' must be equal",
          "to the length of argument 'alpha'" ) )
-   } else if(  length( alpha ) != length( W ) ) {
+   } else if(  length( coef$alpha ) != length( W ) ) {
       stop( "Arguments 'alpha' and 'W' must have the same length." )
-   } else if(  length( alpha ) != length( P ) && !is.null( P ) ) {
+   } else if(  length( coef$alpha ) != length( P ) && !is.null( P ) ) {
       stop( "Arguments 'alpha' and 'P' must have the same length." )
    }
 
@@ -23,7 +23,7 @@ aidsEla <- function( alpha, beta, gamma, W, P = NULL, formula = "AIDS" ) {
       }
    }
 
-   nGoods <- length( alpha )
+   nGoods <- length( coef$alpha )
 
    ela <- list()
    ela$formula <- formula
@@ -32,39 +32,39 @@ aidsEla <- function( alpha, beta, gamma, W, P = NULL, formula = "AIDS" ) {
 
    if( formula == "AIDS" ) {
       P <- array(P)
-      ela$exp <- array( 1, c( nGoods ) ) + beta/W
+      ela$exp <- array( 1, c( nGoods ) ) + coef$beta/W
       ela$hicks <- -diag( 1, nGoods, nGoods ) +
          array( 1, c( nGoods )) %*% t( W ) +
-         gamma / ( W %*% t( array( 1, c( nGoods )))) -
-         beta %*% t( array( 1, c( nGoods ))) *
-         ( array( 1, c( nGoods )) %*% t( alpha ) -
+         coef$gamma / ( W %*% t( array( 1, c( nGoods )))) -
+         coef$beta %*% t( array( 1, c( nGoods ))) *
+         ( array( 1, c( nGoods )) %*% t( coef$alpha ) -
          array( 1, c( nGoods )) %*% t( W )+
-         array( 1, c( nGoods )) %*% t( gamma %*% log( P ))) /
+         array( 1, c( nGoods )) %*% t( coef$gamma %*% log( P ))) /
          ( W %*% t( array( 1, c( nGoods ))))
       ela$marshall <- ela$hicks - ( ela$exp %*% t( array( 1, c( nGoods )))) *
          ( array( 1, c( nGoods )) %*% t( W ))
    } else if(formula=="Ch") {
-      ela$exp <- array( 1, c( nGoods ) ) + beta / W
-      ela$hicks <- -diag( 1, nGoods, nGoods ) + gamma /
+      ela$exp <- array( 1, c( nGoods ) ) + coef$beta / W
+      ela$hicks <- -diag( 1, nGoods, nGoods ) + coef$gamma /
          ( W %*% t( array( 1, c( nGoods ) ) ) ) +
          array( 1, c( nGoods )) %*% t( W )
       ela$marshall <- ela$hicks - ( ela$exp %*% t( array( 1, c( nGoods ) ) ) ) *
          ( array( 1, c( nGoods )) %*% t(W))
    } else if( formula == "EU" ) {
-      ela$exp <- array( 1, c( nGoods ) ) + beta / W
-      ela$marshall <- -diag( 1, nGoods, nGoods ) + gamma /
+      ela$exp <- array( 1, c( nGoods ) ) + coef$beta / W
+      ela$marshall <- -diag( 1, nGoods, nGoods ) + coef$gamma /
          ( W %*% t( array( 1, c( nGoods ) ) ) )
       ela$hicks <- ela$marshall + ( ela$exp %*% t( array( 1, c( nGoods ) ) ) ) *
          ( array( 1, c( nGoods )) %*% t(W))
    } else if( formula == "B1 not implemente" ) {
-      ela$exp <- array( 1, c( nGoods ) ) + beta / W
+      ela$exp <- array( 1, c( nGoods ) ) + coef$beta / W
       ela$marshall <- matrix( NA, nGoods, nGoods )
       for( i in 1:nGoods ) {
          for( j in 1:nGoods ) {
-            ela$marshall[ i, j ] <- -( i == j ) + gamma[ i, j ] / W[ i ] -
-               beta[ i ] * W[ j ] / W[ i ]
+            ela$marshall[ i, j ] <- -( i == j ) + coef$gamma[ i, j ] / W[ i ] -
+               coef$beta[ i ] * W[ j ] / W[ i ]
             for( k in 1:nGoods ) {
-               ela$marshall[ i, j ] <- ela$marshall[ i, j ] - beta[ i ] *
+               ela$marshall[ i, j ] <- ela$marshall[ i, j ] - coef$beta[ i ] *
                   W[ k ] * log( P[ k ] ) * ( 0 + ( k == j ) ) / W[ i ]
             }
          }
