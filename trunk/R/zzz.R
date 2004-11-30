6bb90d4bd292224477f881eb7a832a2b75144e53
suapm095 <suapm095@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2004-11-30 14:24:11 +0000

    changed text displayed on load slightly
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@70 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/zzz.R
+++ trunk/R/zzz.R
@@ -2,7 +2,7 @@
 .First.lib <- function ( ... ) {
    require( systemfit )
 
-   cat( paste( "This package is still in an alpha version.",
+   cat( paste( "This package is still an alpha version.",
       "This means that this package has not been thoroughly tested yet.",
       "Thus, there might be several bugs in it and",
       "I do not guarantee that all results are correct.",
