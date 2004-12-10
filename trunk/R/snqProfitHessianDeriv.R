ad2e01fb09b6bc29629c9ddc610e42b0bc7579d5
suapm095 <unknown@noreply.com>
2004-12-10 13:37:06 +0000

    snqProfitHessianDeriv now also works for form = 1
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@81 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/snqProfitHessianDeriv.R
+++ trunk/R/snqProfitHessianDeriv.R
@@ -1,10 +1,17 @@
 ## --- derivatives of the Hessian with respect to the betas ---
-snqProfitHessianDeriv <- function( prices, weights, nFix = 0 ) {
+snqProfitHessianDeriv <- function( prices, weights, nFix = 0, form = 0 ) {
    prices   <- array( prices )
    weights <- array( weights )
    nNetput  <- dim(array(prices))
-   nCoef    <- nNetput + nNetput * ( nNetput - 1 )/2 + nNetput * nFix +
-      ( nFix + 1 ) * nFix/2    #number of coefficients
+   nCoef    <- nNetput + nNetput * ( nNetput - 1 )/2 + nNetput * nFix
+      # number of coefficients
+   if( form == 0 ) {
+      nCoef <- nCoef + ( nFix + 1 ) * nFix / 2
+   } else if ( form == 1 ) {
+      nCoef <- nCoef + nNetput * ( nFix + 1 ) * nFix / 2
+   } else {
+      stop( "argument 'form' must be either 0 or 1" )
+   }
    normPrice <- sum( t( prices ) %*% weights )
    Hderiv    <- array( 0, c( nNetput * ( nNetput - 1 )/2, nCoef ) )
    kro       <- diag( 1, nNetput, nNetput )    #identity matrix for Kronecker Delta
