92d21dcced4067e13d4c61be85f2589ec5790f7e
suapm095 <suapm095@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2005-02-17 17:51:11 +0000

    heckit.R + heckit.Rd: added calculation of unbiased standard errors and covariance matrix of coefficients
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@93 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/heckit.R
+++ trunk/R/heckit.R
@@ -41,22 +41,62 @@ heckit <- function( formula, probitformula, data ) {
 
    result$lm <- lm( step2formula, data, data$probitdummy == 1 )
 
-   result$sigma <- sqrt( crossprod( residuals( result$lm ) ) /
+   result$sigma <- as.numeric( sqrt( crossprod( residuals( result$lm ) ) /
       sum( data$probitdummy == 1 ) +
       mean( data$probitDelta[ data$probitdummy == 1 ] ) *
-      coefficients( result$lm )[ "probitLambda" ]^2 )
+      coefficients( result$lm )[ "probitLambda" ]^2 ) )
 
    result$rho <- coefficients( result$lm )[ "probitLambda" ] / result$sigma
    result$probitLambda <- data$probitLambda
+   result$probitDelta  <- data$probitDelta
+
+   # the foolowing variables are named according to Greene (2003), p. 785
+   xMat <- model.matrix( result$lm )
+   wMat <- model.matrix( result$probit )[ data$probitdummy == 1, ]
+   fMat <- t( xMat ) %*% diag( result$probitDelta[
+      data$probitdummy == 1 ] ) %*% wMat
+   qMat <- result$rho^2 * ( fMat %*% vcov( result$probit )%*% t( fMat ) )
+   result$vcov <- result$sigma^2 * solve( crossprod( xMat ) ) %*%
+      ( t( xMat ) %*% diag( 1 - result$rho^2 *
+      result$probitDelta[ data$probitdummy == 1 ] ) %*%
+      xMat + qMat ) %*% solve( crossprod( xMat ) )
+   result$coef <- matrix( NA, nrow = length( coef( result$lm ) ), ncol = 4 )
+   rownames( result$coef ) <- names( coef( result$lm ) )
+   colnames( result$coef ) <- c( "Estimate", "Std. Error", "t value",
+      "Pr(>|t|)" )
+   result$coef[ , 1 ] <- coef( result$lm )
+   result$coef[ , 2 ] <- sqrt( diag( result$vcov ) )
+   result$coef[ , 3 ] <- result$coef[ , 1 ] / result$coef[ , 2 ]
+   result$coef[ , 4 ] <- 2 * ( 1 - pt( abs( result$coef[ , 3 ] ),
+      result$lm$df ) )
 
    class( result ) <- "heckit"
    return( result )
 }
 
 summary.heckit <- function( object, ... ) {
-   cat( "1st step probit estimation:\n" )
-   print( summary( object$probit, ... ) )
-   cat( "\n2nd step linear estimation:\n" )
-   print( summary( object$lm, ... ) )
+   print( object, ... )
    invisible( object )
+}
+
+print.heckit <- function( x, digits = 6, ... ) {
+   Signif <- symnum( x$coef[ , 4 ], corr = FALSE, na = FALSE,
+      cutpoints = c( 0, 0.001, 0.01, 0.05, 0.1, 1 ),
+      symbols   = c( "***", "**", "*", "." ," " ))
+
+   table <- cbind( round( x$coef, digits ), Signif )
+
+   rownames( table ) <- rownames( x$coef )
+   colnames( table ) <- c( "Estimate", "Std. Error", "t value",
+      "Pr(>|t|)", "" )
+
+   print.matrix( table, quote = FALSE, right = TRUE )
+   cat( "---\nSignif. codes: ", attr( Signif, "legend" ), "\n" )
+
+   cat( paste(
+      "Multiple R-Squared:", round( summary( x$lm )$r.squared, digits),
+      "Adjusted R-Squared:", round( summary( x$lm )$adj.r.squared, digits),
+      "\n" ) )
+   cat("\n")
+   invisible( x )
 }
