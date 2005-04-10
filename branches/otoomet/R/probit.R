7161eeafc579c6ec28108872660e56219e55afdb
otoomet <otoomet@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2005-04-10 17:21:59 +0000

    probit: added model matrix/frame/response & friends.  Mostly cloned from 'lm' coef method for maxLik model.frame and model.matrix methods for probit
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@131 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- branches/otoomet/R/probit.R
+++ branches/otoomet/R/probit.R
@@ -1,10 +1,13 @@
 ### -*- ess-indent-level: 3 -*-
 probit <- function( formula, b0=NULL, data=sys.frame(sys.parent()),
+                   x=FALSE, y=FALSE, model=FALSE,
+                   method="ML",
                    ...) {
    ## Probit binary choice model
    ## formula: model formula, response must be either a logical or numeric vector containing only 0-s and
    ##          1-s 
    ## b0:      initial value of the parameters
+   ## x        whether to return model matrix
    ## ...      further arguments for the maxLik algorithm
    ##
    ## return: a list with following components.
@@ -12,7 +15,9 @@ probit <- function( formula, b0=NULL, data=sys.frame(sys.parent()),
    ##  $LRT:     list with two components:
    ##            LRT: likelihood ration test H0 - none of the variables significant
    ##            df:  corresponding degrees of freedom
-   ##
+   ##  x         model matrix (only if requested)
+   ##  call      call
+   ##  terms     terms
    loglik <- function( beta) {
       xb0 <- x0 %*% beta
       xb1 <- x1 %*% beta
@@ -36,22 +41,35 @@ probit <- function( formula, b0=NULL, data=sys.frame(sys.parent()),
          t( x0) %*% ( x0 * ( -f0*xb0*yF0 + f0*f0)/yF0/yF0)
                     # note that df/db' = -f (x'b) x'
    }
-   x <- model.matrix(formula, data=data)
-   y <- as.numeric(model.response( model.frame( formula, data=data)))
-   NParam <- ncol( x)
-   NObs <- length( y)
-   N1 <- length( y[y != 0])
+   cl <- match.call()
+   mf <- match.call(expand.dots = FALSE)
+   m <- match(c("formula", "data", "subset", "weights", "na.action", 
+                "offset"), names(mf), 0)
+   mf <- mf[c(1, m)]
+   mf$drop.unused.levels <- TRUE
+   mf[[1]] <- as.name("model.frame")
+   mf <- eval(mf, parent.frame())
+   if (method == "model.frame") 
+       return(mf)
+   else if (method != "ML") 
+       warning("method = ", method, " is not supported. Using \"qr\".")
+   mt <- attr(mf, "terms")
+   Y <- model.response(mf, "numeric")
+   X <- model.matrix(mt, mf, contrasts)
+   NParam <- ncol( X)
+   NObs <- length( Y)
+   N1 <- length( y[Y != 0])
    N0 <- NObs - N1
    if(N0 == 0 | N1 == 0) {
       stop("only zero or one observations")
    }
-   x0 <- x[y==0,]
-   x1 <- x[y==1,]
+   x0 <- X[Y==0,]
+   x1 <- X[Y==1,]
    if(is.null(b0)) {
       b0 <- rep( 0, NParam)
    }
    if(is.null(names(b0))) {
-      names(b0) <- dimnames(x)[[2]]
+      names(b0) <- dimnames(X)[[2]]
    }
    ## Main estimation
    estimation <- maxLik(loglik, gradlik, hesslik, b0,
@@ -63,13 +81,22 @@ probit <- function( formula, b0=NULL, data=sys.frame(sys.parent()),
    ll.bar <- loglik(c(qnorm(N1/NObs), rep(0, NParam-1)))
    LRT <- 2*(estimation$maximum - ll.bar)
                                         #
+   ## linear predictors (glm clone)
+   linear.predictors <- X %*% coefficients(estimation)
+                                        #
    result <- c(estimation,
                LRT=list(list(LRT=LRT, df=NParam-1)),
+               linear.predictors=list(linear.predictors),
                NParam=NParam,
                NObs=NObs,
                N1=N1,
                N0=N0,
-               df=NObs - NParam)
+               df=NObs - NParam,
+               call=cl,
+               terms=mt,
+               x=switch(x, "1"=X, "0"=NULL),
+               y=switch(y, "1"=Y, "0"=NULL),
+               model=switch(model, "1"=mf, "0"=NULL))
                                         # there are df-1 constraints
    class(result) <- c("probit", class(estimation))
    result
