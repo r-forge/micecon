8ee705dcea5fe94fa58f83b3a66200619bedcaf5
suapm095 <suapm095@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2005-05-25 08:33:01 +0000

    branches/otoomet/R/invMillsRatio.R: changed 'selection' for univariate cases of bivariate probit (corrected bug for IMR0X and IMRX0 if all == TRUE); added 'deltas' for univariate cases of bivariate probit (delta1X, delta0X, deltaX1, deltaX0)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@160 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- branches/otoomet/R/invMillsRatio.R
+++ branches/otoomet/R/invMillsRatio.R
@@ -11,7 +11,7 @@ invMillsRatio <- function( x, all = FALSE ) {
          pnorm( x$linear.predictors )
       result$delta1 <- result$IMR1 * ( result$IMR1 + x$linear.predictors )
       result$IMR0 <- dnorm( x$linear.predictors ) /
-         pnorm( -x$linear.predictors ) 
+         pnorm( -x$linear.predictors )
       result$delta0 <- result$IMR0 * ( result$IMR0 + x$linear.predictors )
    } else if( class( x ) == "vglm" ) {
       library( VGAM )
@@ -82,30 +82,52 @@ invMillsRatio <- function( x, all = FALSE ) {
       result$IMR00b <- -dnorm( x@predictors[ , 2 ] ) *
          pnorm( ( -x@predictors[ , 1 ] + rho * x@predictors[ , 2 ] ) /
             ( 1 - rho^2 )^0.5 ) / pmvnormValues00
+
+      if( all ) {
+         selection1X <- rep( TRUE, nrow( result ) )
+         selection0X <- rep( TRUE, nrow( result ) )
+         selectionX1 <- rep( TRUE, nrow( result ) )
+         selectionX0 <- rep( TRUE, nrow( result ) )
+      } else {
+         selection1X <- ( x@y[ , "11" ] + x@y[ , "10" ] ) == 1
+         selection0X <- ( x@y[ , "01" ] + x@y[ , "00" ] ) == 1
+         selectionX1 <- ( x@y[ , "11" ] + x@y[ , "01" ] ) == 1
+         selectionX0 <- ( x@y[ , "10" ] + x@y[ , "00" ] ) == 1
+      }
+
       # only considering the first probit equation
       result$IMR1X <- NA
+      result$IMR1X[ selection1X ] <- dnorm( x@predictors[ selection1X, 1 ] ) /
+         pnorm( x@predictors[ selection1X, 1 ] )
+
+      result$delta1X <- NA
+      result$delta1X[ selection1X ] <- result$IMR1X *
+         ( result$IMR1X + x$predictors[ selection1X, 1 ] )
+
       result$IMR0X <- NA
-      if( all ) {
-         selection <- rep( TRUE, nrow( result ) )
-      } else {
-         selection <- ( x@y[ , "11" ] + x@y[ , "10" ] ) == 1
-      }
-      result$IMR1X[ selection ] <- dnorm( x@predictors[ selection, 1 ] ) / 
-         pnorm( x@predictors[ selection, 1 ] ) 
-      result$IMR0X[ !selection ] <- -dnorm( x@predictors[ !selection, 1 ] ) / 
-         pnorm( -x@predictors[ !selection, 1 ] ) 
+      result$IMR0X[ selection0X ] <- -dnorm( x@predictors[ selection0X, 1 ] ) /
+         pnorm( -x@predictors[ selection0X, 1 ] )
+      result$delta0X <- NA
+      result$delta0X[ selection0X ] <- result$IMR0X *
+         ( result$IMR0X + x$predictors[ selection0X, 1 ] )
+
       # only considering the second probit equation
       result$IMRX1 <- NA
+      result$IMRX1[ selectionX1 ] <- dnorm( x@predictors[ selectionX1, 2 ] ) /
+         pnorm( x@predictors[ selectionX1, 2 ] )
+
+      result$deltaX1 <- NA
+      result$deltaX1[ selectionX1 ] <- result$IMRX1 *
+         ( result$IMRX1 + x$predictors[ selectionX1, 2 ] )
+
       result$IMRX0 <- NA
-      if( all ) {
-         selection <- rep( TRUE, nrow( result ) )
-      } else {
-         selection <- ( x@y[ , "11" ] + x@y[ , "01" ] ) == 1
-      }
-      result$IMRX1[ selection ] <- dnorm( x@predictors[ selection, 2 ] ) / 
-         pnorm( x@predictors[ selection, 2 ] ) 
-      result$IMRX0[ !selection ] <- -dnorm( x@predictors[ !selection, 2 ] ) / 
-         pnorm( -x@predictors[ !selection, 2 ] ) 
+      result$IMRX0[ selectionX0 ] <- -dnorm( x@predictors[ selectionX0, 2 ] ) /
+         pnorm( -x@predictors[ selectionX0, 2 ] )
+
+      result$deltaX0 <- NA
+      result$deltaX0[ selectionX0 ] <- result$IMRX0 *
+         ( result$IMRX0 + x$predictors[ selectionX0, 2 ] )
+
    } else {
       stop( errorMessage )
    }
