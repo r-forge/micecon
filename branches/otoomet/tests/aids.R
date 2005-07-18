ce3fa481a0b975a04cd79bc4686a4af1f8f7f730
suapm095 <suapm095@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2005-07-18 10:35:58 +0000

    branches/otoomet/tests/aids.R(out.save): added several further tests of AIDS model: unrestricted estimation, estimation with only homogeneity imposed (no symmetry), imposing restrictions via TX
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@184 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- branches/otoomet/tests/aids.R
+++ branches/otoomet/tests/aids.R
@@ -13,7 +13,62 @@ estResultLA <- aidsEst( pNames, wNames, "xFood",
    data = Blanciforti86, method = "LA:SL", elaFormula = "Ch",
    maxiter = 1, rcovformula = 1, tol = 1e-7 )
 print( estResultLA )
+# imposing reestrictions via TX
+estResultLATX <- aidsEst( pNames, wNames, "xFood",
+   data = Blanciforti86, method = "LA:SL", elaFormula = "Ch",
+   maxiter = 1, rcovformula = 1, tol = 1e-7, TX = TRUE )
+print( estResultLATX )
+estResultLATX$est$bt <- NULL
+estResultLATX$est$btcov <- NULL
+estResultLATX$est$x <- NULL
+estResultLATX$est$TX <- NULL
+estResultLATX$est$q.restr <- NULL
+estResultLA$est$x <- NULL
+estResultLA$est$R.restr <- NULL
+estResultLA$est$q.restr <- NULL
+print( all.equal( estResultLA, estResultLATX ) )
 
+## only homogeneity (no symmetry imposed)
+estResultLAhom <- aidsEst( pNames, wNames, "xFood", sym = FALSE,
+   data = Blanciforti86, method = "LA:SL", elaFormula = "Ch",
+   maxiter = 1, rcovformula = 1, tol = 1e-7 )
+print( estResultLAhom )
+# imposing reestrictions via TX
+estResultLAhomTX <- aidsEst( pNames, wNames, "xFood", sym = FALSE,
+   data = Blanciforti86, method = "LA:SL", elaFormula = "Ch",
+   maxiter = 1, rcovformula = 1, tol = 1e-7, TX = TRUE )
+print( estResultLAhomTX )
+estResultLAhomTX$est$bt <- NULL
+estResultLAhomTX$est$btcov <- NULL
+estResultLAhomTX$est$x <- NULL
+estResultLAhomTX$est$TX <- NULL
+estResultLAhomTX$est$q.restr <- NULL
+estResultLAhom$est$x <- NULL
+estResultLAhom$est$R.restr <- NULL
+estResultLAhom$est$q.restr <- NULL
+print( all.equal( estResultLAhom, estResultLAhomTX ) )
+
+## unrestricted (no homogeneity and no symmetry imposed)
+estResultLAunr <- aidsEst( pNames, wNames, "xFood", hom = FALSE, sym = FALSE,
+   data = Blanciforti86, method = "LA:SL", elaFormula = "Ch",
+   maxiter = 1, rcovformula = 1, tol = 1e-7 )
+print( estResultLAunr )
+# imposing reestrictions via TX
+estResultLAunrTX <- aidsEst( pNames, wNames, "xFood", hom = FALSE, sym = FALSE,
+   data = Blanciforti86, method = "LA:SL", elaFormula = "Ch",
+   maxiter = 1, rcovformula = 1, tol = 1e-7, TX = TRUE )
+print( estResultLAunrTX )
+estResultLAunrTX$est$bt <- NULL
+estResultLAunrTX$est$btcov <- NULL
+estResultLAunrTX$est$x <- NULL
+estResultLAunrTX$est$TX <- NULL
+estResultLAunrTX$est$q.restr <- NULL
+estResultLAunr$est$x <- NULL
+estResultLAunr$est$R.restr <- NULL
+estResultLAunr$est$q.restr <- NULL
+print( all.equal( estResultLAunr, estResultLAunrTX ) )
+
+#####################################################
 cat( paste( "\nRepeating the evaluation of different elasticity formulas",
    "of Green & Alston (1990): iterated AIDS\n" ) )
 estResultAIDS <- aidsEst( pNames, wNames, "xFood",
@@ -21,17 +76,83 @@ estResultAIDS <- aidsEst( pNames, wNames, "xFood",
    elaFormula = "AIDS", rcovformula=1, tol=1e-7,
    method = "MK:L" )
 print( estResultAIDS )
+# imposing reestrictions via TX
+estResultAIDSTX <- aidsEst( pNames, wNames, "xFood",
+   data = Blanciforti86[ -1, ], maxiter = 1,
+   elaFormula = "AIDS", rcovformula=1, tol=1e-7,
+   method = "MK:L", TX = TRUE )
+print( estResultAIDSTX )
+estResultAIDSTX$est$bt <- NULL
+estResultAIDSTX$est$btcov <- NULL
+estResultAIDSTX$est$x <- NULL
+estResultAIDSTX$est$TX <- NULL
+estResultAIDSTX$est$q.restr <- NULL
+estResultAIDS$est$x <- NULL
+estResultAIDS$est$R.restr <- NULL
+estResultAIDS$est$q.restr <- NULL
+print( all.equal( estResultAIDS, estResultAIDSTX ) )
+
+## only homogeneity (no symmetry imposed)
+estResultAIDShom <- aidsEst( pNames, wNames, "xFood", sym = FALSE,
+   data = Blanciforti86[ -1, ], maxiter = 1,
+   elaFormula = "AIDS", rcovformula=1, tol=1e-7,
+   method = "MK:L" )
+print( estResultAIDShom )
+# imposing reestrictions via TX
+estResultAIDShomTX <- aidsEst( pNames, wNames, "xFood", sym = FALSE,
+   data = Blanciforti86[ -1, ], maxiter = 1,
+   elaFormula = "AIDS", rcovformula=1, tol=1e-7,
+   method = "MK:L", TX = TRUE )
+print( estResultAIDShomTX )
+estResultAIDShomTX$est$bt <- NULL
+estResultAIDShomTX$est$btcov <- NULL
+estResultAIDShomTX$est$x <- NULL
+estResultAIDShomTX$est$TX <- NULL
+estResultAIDShomTX$est$q.restr <- NULL
+estResultAIDShom$est$x <- NULL
+estResultAIDShom$est$R.restr <- NULL
+estResultAIDShom$est$q.restr <- NULL
+print( all.equal( estResultAIDShom, estResultAIDShomTX ) )
+
+## unrestricted (no homogeneity and no symmetry imposed)
+estResultAIDSunr <- aidsEst( pNames, wNames, "xFood", hom = FALSE, sym = FALSE,
+   data = Blanciforti86[ -1, ], maxiter = 1,
+   elaFormula = "AIDS", rcovformula=1, tol=1e-7,
+   method = "MK:L" )
+print( estResultAIDSunr )
+# imposing reestrictions via TX
+estResultAIDSunrTX <- aidsEst( pNames, wNames, "xFood", hom = FALSE, sym = FALSE,
+   data = Blanciforti86[ -1, ], maxiter = 1,
+   elaFormula = "AIDS", rcovformula=1, tol=1e-7,
+   method = "MK:L", TX = TRUE )
+print( estResultAIDSunrTX )
+estResultAIDSunrTX$est$bt <- NULL
+estResultAIDSunrTX$est$btcov <- NULL
+estResultAIDSunrTX$est$x <- NULL
+estResultAIDSunrTX$est$TX <- NULL
+estResultAIDSunrTX$est$q.restr <- NULL
+estResultAIDSunr$est$x <- NULL
+estResultAIDSunr$est$R.restr <- NULL
+estResultAIDSunr$est$q.restr <- NULL
+print( all.equal( estResultAIDSunr, estResultAIDSunrTX ) )
 
 ########## Elasticities ###############
 cat( "\nAIDS: Elasticities\n" )
 wMeans <- colMeans( Blanciforti86[ , wNames ] )
 ela <- aidsEla( estResultAIDS$coef, wMeans, pMeans, formula = "AIDS" )
 print( ela )
+elaTX <- aidsEla( estResultAIDSTX$coef, wMeans, pMeans, formula = "AIDS" )
+print( elaTX )
+print( all.equal( ela, elaTX ) )
+
 
 cat( "\nLA: Elasticity formula of non-linear AIDS\n" )
 wMeans <- colMeans( Blanciforti86[ , wNames ] )
 ela <- aidsEla( estResultLA$coef, wMeans, pMeans, formula = "AIDS" )
 print( ela )
+elaTX <- aidsEla( estResultLATX$coef, wMeans, pMeans, formula = "AIDS" )
+print( elaTX )
+print( all.equal( ela, elaTX ) )
 
 cat( "\n********** Elasticities ***************" )
 cat( "\nLA: Elasticity formula of Chalfant / Goddard\n" )
@@ -81,6 +202,10 @@ if( max( abs( fittedAIDS$shares - estResultAIDS$wFitted ) ) > 1e-5 ) {
 if( max( abs( fittedAIDS$quant - estResultAIDS$qFitted ) ) > 1e-5 ) {
    stop( "fitted quantities of AIDS are wrong" )
 }
+fittedAIDSTX <- aidsCalc( pNames, "xFood", Blanciforti86[ -1, ],
+   coef = estResultAIDSTX$coef )
+print( fittedAIDSTX )
+print( all.equal( fittedAIDS, fittedAIDSTX ) )
 
 fittedLA <- aidsCalc( pNames, "xFood", Blanciforti86,
    coef = estResultLA$coef, lnp = estResultLA$lnp )
@@ -91,6 +216,10 @@ if( max( abs( fittedLA$shares[ -1, ] - estResultLA$wFitted[ -1, ] ) ) > 1e-5 ) {
 if( max( abs( fittedLA$quant[ -1, ] - estResultLA$qFitted[ -1, ] ) ) > 1e-5 ) {
    stop( "fitted quantities of LA-AIDS are wrong" )
 }
+fittedLATX <- aidsCalc( pNames, "xFood", Blanciforti86,
+   coef = estResultLATX$coef, lnp = estResultLATX$lnp )
+print( fittedLATX )
+print( all.equal( fittedLA, fittedLATX ) )
 
 ####### consistency ###################
 consist <- aidsTestConsist( pNames, wNames, "xFood", Blanciforti86,
