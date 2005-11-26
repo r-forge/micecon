d5fb3f357de3477995f4a355cd37b23ff2007a7f
suapm095 <suapm095@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2005-11-26 13:30:10 +0000

    trunk/*/aidsEla.R(d): added calculation of variance covariance matrices of AIDS elasticities
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@226 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/aidsEla.R
+++ trunk/R/aidsEla.R
@@ -1,5 +1,5 @@
 aidsEla <- function( coef, W, P = NULL, formula = "AIDS",
-   qNames = NULL, pNames = NULL ) {
+   qNames = NULL, pNames = NULL, coefVcov = NULL, df = NULL ) {
 
    if( length( coef$alpha ) != length( coef$beta ) ) {
       stop( "arguments 'alpha' and 'beta' must have the same length" )
@@ -57,7 +57,7 @@ aidsEla <- function( coef, W, P = NULL, formula = "AIDS",
          ( W %*% t( array( 1, c( nGoods ) ) ) )
       ela$hicks <- ela$marshall + ( ela$exp %*% t( array( 1, c( nGoods ) ) ) ) *
          ( array( 1, c( nGoods )) %*% t(W))
-   } else if( formula == "B1 not implemente" ) {
+   } else if( formula == "B1 not implemented" ) {
       ela$exp <- array( 1, c( nGoods ) ) + coef$beta / W
       ela$marshall <- matrix( NA, nGoods, nGoods )
       for( i in 1:nGoods ) {
@@ -83,6 +83,36 @@ aidsEla <- function( coef, W, P = NULL, formula = "AIDS",
    if( !is.null( pNames ) ) {
       colnames( ela$hicks ) <- pNames
       colnames( ela$marshall ) <- pNames
+   }
+   if( !is.null( coefVcov ) && formula %in% c( "AIDS" ) ) {
+      jacobian <- aidsElaJacobian( coef = coef, share = W, price = P, 
+         formula = formula )
+      ela$allVcov      <- jacobian$all      %*% coefVcov %*% t( jacobian$all )
+      ela$expVcov      <- jacobian$exp      %*% coefVcov %*% t( jacobian$exp )
+      ela$hicksVcov    <- jacobian$hicks    %*% coefVcov %*% t( jacobian$hicks )
+      ela$marshallVcov <- jacobian$marshall %*% coefVcov %*% t( jacobian$marshall )
+      # standard errors
+      ela$expStEr      <- diag( ela$expVcov )^0.5
+      ela$hicksStEr    <- matrix( diag( ela$hicksVcov )^0.5, 
+         ncol = nGoods, byrow = TRUE )
+      ela$marshallStEr <-  matrix( diag( ela$marshallVcov )^0.5, 
+         ncol = nGoods, byrow = TRUE )
+      # dim names for standard errors
+      names( ela$expStEr )         <- names( ela$exp )
+      dimnames( ela$hicksStEr )    <- dimnames( ela$hicks )
+      dimnames( ela$marshallStEr ) <- dimnames( ela$marshall )
+      # t-values
+      ela$expTval      <- ela$exp      / ela$expStEr
+      ela$hicksTval    <- ela$hicks    / ela$hicksStEr
+      ela$marshallTval <- ela$marshall / ela$marshallStEr
+      if( !is.null( df ) ) {
+         ela$expPval <- 2 * pt( abs( ela$expTval ), df, 
+            lower.tail = FALSE )
+         ela$hicksPval <- 2 * pt( abs( ela$hicksTval ), df, 
+            lower.tail = FALSE )
+         ela$marshallPval <- 2 * pt( abs( ela$marshallTval ), df, 
+            lower.tail = FALSE )
+      }
    }
    class( ela ) <- "aidsEla"
    return( ela )
--- trunk/R/aidsElaJacobian.R
+++ trunk/R/aidsElaJacobian.R
@@ -22,12 +22,12 @@ aidsElaJacobian <- function( coef, share, price = NULL, formula = "AIDS" ) {
    nGoods <- length( coef$alpha )
    nCoef  <- length( coef$all )
 
-   createMatrix <- function( nGoods, nCoef, dim ) {
+   createMatrix <- function( nGoods, nCoef, dim, symbol ) {
       result <- matrix( 0, nrow = nGoods^dim, ncol = nCoef )
       if( dim == 1 ) {
-         rownames( result ) <- paste( "E", c( 1:nGoods ) )
+         rownames( result ) <- paste( symbol, c( 1:nGoods ) )
       } else if( dim == 2 ) {
-         rownames( result ) <- paste( "E", rep( 1:nGoods, each = nGoods ),
+         rownames( result ) <- paste( symbol, rep( 1:nGoods, each = nGoods ),
             rep( 1:nGoods, nGoods ) )
       }
       colnames( result ) <- names( coef$all )
@@ -36,9 +36,9 @@ aidsElaJacobian <- function( coef, share, price = NULL, formula = "AIDS" ) {
 
    jacobian <- list()
    jacobian$formula  <- formula
-   jacobian$exp      <- createMatrix( nGoods, nCoef, 1 )
-   jacobian$hicks    <- createMatrix( nGoods, nCoef, 2 )
-   jacobian$marshall <- createMatrix( nGoods, nCoef, 2 )
+   jacobian$exp      <- createMatrix( nGoods, nCoef, 1, "Ex" )
+   jacobian$hicks    <- createMatrix( nGoods, nCoef, 2, "Eh" )
+   jacobian$marshall <- createMatrix( nGoods, nCoef, 2, "Em" )
 
    share <- array( share )
 
@@ -46,35 +46,37 @@ aidsElaJacobian <- function( coef, share, price = NULL, formula = "AIDS" ) {
    bName <- paste( "beta", c( 1:nGoods ) )
    gName <- array( paste( "gamma", rep( 1:nGoods, nGoods ),
       rep( 1:nGoods, each = nGoods ) ), dim = c( nGoods, nGoods ) )
-   eName <- array( paste( "E", rep( 1:nGoods, nGoods ),
+   ehName <- array( paste( "Eh", rep( 1:nGoods, nGoods ),
+      rep( 1:nGoods, each = nGoods ) ), dim = c( nGoods, nGoods ) )
+   emName <- array( paste( "Em", rep( 1:nGoods, nGoods ),
       rep( 1:nGoods, each = nGoods ) ), dim = c( nGoods, nGoods ) )
 
    if( formula == "AIDS" ) {
       price <- array( price )
       for( i in 1:nGoods ) {
          # expenditure elasticities
-         jacobian$exp[ paste( "E", i ), bName[ i ] ] <-
+         jacobian$exp[ paste( "Ex", i ), bName[ i ] ] <-
             1 / share[ i ]
          for( j in 1:nGoods ) {
             # Hicksian price elasticities
-            jacobian$hicks[ eName[ i, j ], aName[ j ] ] <-
+            jacobian$hicks[ ehName[ i, j ], aName[ j ] ] <-
                -coef$beta[ i ] / share[ i ]
-            jacobian$hicks[ eName[ i, j ], bName[ i ] ] <-
+            jacobian$hicks[ ehName[ i, j ], bName[ i ] ] <-
                - ( coef$alpha[ j ] - share[ j ] +
                coef$gamma[ j , ] %*% log( price ) ) / share[ i ]
             for( k in 1:nGoods ) {
-               jacobian$hicks[ eName[ i, j ], gName[ k, j ] ] <-
+               jacobian$hicks[ ehName[ i, j ], gName[ k, j ] ] <-
                   ( i == k ) / share[ i ] -
                   coef$beta[ i ] * log( price[ k ] ) / share[ i ]
             }
             # Marshallian price elasticities
-            jacobian$marshall[ eName[ i, j ], aName[ j ] ] <-
+            jacobian$marshall[ emName[ i, j ], aName[ j ] ] <-
                -coef$beta[ i ] / share[ i ]
-            jacobian$marshall[ eName[ i, j ], bName[ i ] ] <-
+            jacobian$marshall[ emName[ i, j ], bName[ i ] ] <-
                - ( coef$alpha[ j ] +
                coef$gamma[ j , ] %*% log( price ) ) / share[ i ]
             for( k in 1:nGoods ) {
-               jacobian$marshall[ eName[ i, j ], gName[ k, j ] ] <-
+               jacobian$marshall[ emName[ i, j ], gName[ k, j ] ] <-
                   ( i == k ) / share[ i ] -
                   coef$beta[ i ] * log( price[ k ] ) / share[ i ]
             }
@@ -83,5 +85,6 @@ aidsElaJacobian <- function( coef, share, price = NULL, formula = "AIDS" ) {
    } else {
       stop( "formula '", as.character( formula ), "' is not supported" )
    }
+   jacobian$all <- rbind( jacobian$exp, jacobian$hicks, jacobian$marshall )
    return( jacobian )
 }
