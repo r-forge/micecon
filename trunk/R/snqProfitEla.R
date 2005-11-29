55c702d2eb2fb86c565d07467d5bdf3e9205ff96
suapm095 <unknown@noreply.com>
2005-11-29 12:21:17 +0000

    trunk/R/snqProfit*.R: added real names to variance covariance matrices of elasticities for the SNQ profit function; added functions .snqProfitQuantNames and .snqProfitPriceNames; updated tests/snqProfit.Rout.save
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@230 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/snqProfitEla.R
+++ trunk/R/snqProfitEla.R
@@ -1,5 +1,5 @@
 ## ===== calculation of elasticities from beta matrix ===
-snqProfitEla <- function( beta, prices, quant, weights, 
+snqProfitEla <- function( beta, prices, quant, weights,
    coefVcov = NULL, df = NULL ) {
    if( !is.matrix( beta ) ) {
       stop( "argument 'beta' must be a matrix" )
@@ -24,27 +24,26 @@ snqProfitEla <- function( beta, prices, quant, weights,
    result   <- list()
    result$ela <- hessian * array( 1, c( nNetput ) ) %*% t( prices ) /
                   quant %*% t( array( 1, c( nNetput ) ) )
-   if( !is.null( names( quant ) ) ) {
-      rownames( result$ela ) <- names( quant )
-   } else {
-      rownames( result$ela ) <- paste( "q", 1:nNetput, sep = "" )
-   }
-   if( !is.null( names( prices ) ) ) {
-      colnames( result$ela ) <- names( prices )
-   } else {
-      colnames( result$ela ) <- paste( "p", 1:nNetput, sep = "" )
-   }
+
+   qNames <- .snqProfitQuantNames( quant, nNetput )
+   pNames <- .snqProfitPriceNames( prices, nNetput )
+
+   dimnames( result$ela ) <- list( qNames, pNames )
+
    if( !is.null( coefVcov ) ) {
-      jacobian <- snqProfitElaJacobian( beta, prices, quant, weights ) 
+      jacobian <- snqProfitElaJacobian( beta, prices, quant, weights )
       betaIndex <- grep( "beta", rownames( coefVcov ) )
       betaVcov <- coefVcov[ betaIndex, betaIndex ]
       result$vcov <- jacobian %*% betaVcov %*% t( jacobian )
       result$stEr <- matrix( diag( result$vcov )^0.5, nrow = nNetput,
          byrow = TRUE )
       result$tval <- result$ela / result$stEr
+      dimnames( result$stEr ) <- list( qNames, pNames )
+      dimnames( result$tval ) <- list( qNames, pNames )
       if( !is.null( df ) ) {
-         result$pval <- 2 * pt( abs( result$tval ), df, 
+         result$pval <- 2 * pt( abs( result$tval ), df,
             lower.tail = FALSE )
+         dimnames( result$pval ) <- list( qNames, pNames )
       }
    }
    class( result ) <- "snqProfitEla"
--- trunk/R/snqProfitElaJacobian.R
+++ trunk/R/snqProfitElaJacobian.R
@@ -20,44 +20,46 @@ snqProfitElaJacobian <- function( beta, prices, quant, weights ) {
    prices   <- unlist( prices )
    quant    <- unlist( quant )
    normPrice <- sum( t( prices ) %*% weights )
+   qNames   <- .snqProfitQuantNames( quant, nNetput )
+   pNames   <- .snqProfitPriceNames( prices, nNetput )
 
    jacobian <- matrix( 0, nrow = nNetput^2, ncol = nNetput^2 )
-   rownames( jacobian ) <- paste( "E", rep( 1:nNetput, each = nNetput ),
-      rep( 1:nNetput, nNetput ) )
+   rownames( jacobian ) <- paste( "E", rep( qNames, each = nNetput ),
+      rep( pNames, nNetput ) )
    colnames( jacobian ) <- paste( "beta", rep( 1:nNetput, each = nNetput ),
       rep( 1:nNetput, nNetput ) )
    bName <- array( paste( "beta", rep( 1:nNetput, nNetput ),
       rep( 1:nNetput, each = nNetput ) ), dim = c( nNetput, nNetput ) )
-   eName <- array( paste( "E", rep( 1:nNetput, nNetput ),
-      rep( 1:nNetput, each = nNetput ) ), dim = c( nNetput, nNetput ) )
-      
-   
+   eName <- array( paste( "E", rep( qNames, nNetput ),
+      rep( pNames, each = nNetput ) ), dim = c( nNetput, nNetput ) )
+
+
    for( i in 1:nNetput ) {
       for( j in 1:nNetput ) {
-         jacobian[ eName[ i, j ], bName[ i, j ] ] <- 
+         jacobian[ eName[ i, j ], bName[ i, j ] ] <-
             prices[ j ] / ( quant[ i ] * normPrice )
          for( k in 1:nNetput ) {
-            jacobian[ eName[ i, j ], bName[ i, k ] ] <- 
-               jacobian[ eName[ i, j ], bName[ i, k ] ] - 
+            jacobian[ eName[ i, j ], bName[ i, k ] ] <-
+               jacobian[ eName[ i, j ], bName[ i, k ] ] -
                weights[ j ] * prices[ k ] * prices[ j ] /
                ( quant[ i ] * normPrice^2 )
          }
          for( l in 1:nNetput ) {
-            jacobian[ eName[ i, j ], bName[ j, l ] ] <- 
-               jacobian[ eName[ i, j ], bName[ j, l ] ] - 
+            jacobian[ eName[ i, j ], bName[ j, l ] ] <-
+               jacobian[ eName[ i, j ], bName[ j, l ] ] -
                weights[ i ] * prices[ l ] * prices[ j ] /
                ( quant[ i ] * normPrice^2 )
          }
          for( k in 1:nNetput ) {
             for( l in 1:nNetput ) {
-               jacobian[ eName[ i, j ], bName[ k, l ] ] <- 
+               jacobian[ eName[ i, j ], bName[ k, l ] ] <-
                   jacobian[ eName[ i, j ], bName[ k, l ] ] +
-                  weights[ i ] * weights[ j ] * prices[ k ] * prices[ l ] * 
+                  weights[ i ] * weights[ j ] * prices[ k ] * prices[ l ] *
                   prices[ j ] / ( quant[ i ] * normPrice^3 )
             }
          }
       }
-   } 
+   }
 #    # test: compare with  results of snqProfitHessianDeriv(  )
 #    hessianDeriv <- jacobian
 #    for( i in 1:nNetput ) {
@@ -66,14 +68,14 @@ snqProfitElaJacobian <- function( beta, prices, quant, weights ) {
 #             quant[ i ] / prices[ j ]
 #       }
 #    }
-#    hessianDeriv[ , "beta 1 1" ] <- hessianDeriv[ , "beta 1 1" ] - 
+#    hessianDeriv[ , "beta 1 1" ] <- hessianDeriv[ , "beta 1 1" ] -
 #       hessianDeriv[ , "beta 1 3" ] - hessianDeriv[ , "beta 3 1" ] +
 #       hessianDeriv[ , "beta 3 3" ]
-#    hessianDeriv[ , "beta 1 2" ] <- hessianDeriv[ , "beta 1 2" ] - 
+#    hessianDeriv[ , "beta 1 2" ] <- hessianDeriv[ , "beta 1 2" ] -
 #       hessianDeriv[ , "beta 1 3" ] + hessianDeriv[ , "beta 2 1" ] -
 #       hessianDeriv[ , "beta 2 3" ] - hessianDeriv[ , "beta 3 1" ] -
 #       hessianDeriv[ , "beta 3 2" ] + 2 * hessianDeriv[ , "beta 3 3" ]
-#    hessianDeriv[ , "beta 2 2" ] <- hessianDeriv[ , "beta 2 2" ] - 
+#    hessianDeriv[ , "beta 2 2" ] <- hessianDeriv[ , "beta 2 2" ] -
 #       hessianDeriv[ , "beta 2 3" ] - hessianDeriv[ , "beta 3 2" ] +
 #       hessianDeriv[ , "beta 3 3" ]
 #    return( hessianDeriv )
