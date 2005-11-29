427f6b6b4e225247bb1a0644aa45c265cc289d32
suapm095 <unknown@noreply.com>
2005-11-29 11:55:56 +0000

    trunk/R/aids*.R: added real names to variance covariance matrices of elasticities for the AIDS; added functions .aidsQuantNames and .aidsPriceNames; updated tests/aids.Rout.save
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@229 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/aidsElaJacobian.R
+++ trunk/R/aidsElaJacobian.R
@@ -1,4 +1,8 @@
-aidsElaJacobian <- function( coef, share, price = NULL, formula = "AIDS" ) {
+aidsElaJacobian <- function( coef, share, price = NULL, formula = "AIDS",
+      qNames = NULL, pNames = NULL ) {
+
+   nGoods <- length( coef$alpha )
+   nCoef  <- length( coef$all )
 
    if( length( coef$alpha ) != length( coef$beta ) ) {
       stop( "arguments 'alpha' and 'beta' must have the same length" )
@@ -12,6 +16,20 @@ aidsElaJacobian <- function( coef, share, price = NULL, formula = "AIDS" ) {
    } else if(  length( coef$alpha ) != length( price ) && !is.null( price ) ) {
       stop( "arguments 'alpha' and 'price' must have the same length" )
    }
+   if( is.null( qNames ) ) {
+      qNames <- .aidsQuantNames( share, coef, nGoods )
+   } else {
+      if( length( qNames ) != nGoods ) {
+         stop( "argument 'qNames' must have ", nGoods, " elements" )
+      }
+   }
+   if( is.null( pNames ) ) {
+      pNames <- .aidsPriceNames( price, coef, nGoods )
+   } else {
+      if( length( pNames ) != nGoods ) {
+         stop( "argument 'pNames' must have ", nGoods, " elements" )
+      }
+   }
 
    if( formula %in% c( "AIDS" ) ) {
       if( is.null( price ) ) {
@@ -19,16 +37,13 @@ aidsElaJacobian <- function( coef, share, price = NULL, formula = "AIDS" ) {
       }
    }
 
-   nGoods <- length( coef$alpha )
-   nCoef  <- length( coef$all )
-
-   createMatrix <- function( nGoods, nCoef, dim, symbol ) {
+   createMatrix <- function( nGoods, nCoef, dim, symbol, qNames, pNames ) {
       result <- matrix( 0, nrow = nGoods^dim, ncol = nCoef )
       if( dim == 1 ) {
-         rownames( result ) <- paste( symbol, c( 1:nGoods ) )
+         rownames( result ) <- paste( symbol, qNames )
       } else if( dim == 2 ) {
-         rownames( result ) <- paste( symbol, rep( 1:nGoods, each = nGoods ),
-            rep( 1:nGoods, nGoods ) )
+         rownames( result ) <- paste( symbol, rep( qNames, each = nGoods ),
+            rep( pNames, nGoods ) )
       }
       colnames( result ) <- names( coef$all )
       return( result )
@@ -36,9 +51,9 @@ aidsElaJacobian <- function( coef, share, price = NULL, formula = "AIDS" ) {
 
    jacobian <- list()
    jacobian$formula  <- formula
-   jacobian$exp      <- createMatrix( nGoods, nCoef, 1, "Ex" )
-   jacobian$hicks    <- createMatrix( nGoods, nCoef, 2, "Eh" )
-   jacobian$marshall <- createMatrix( nGoods, nCoef, 2, "Em" )
+   jacobian$exp      <- createMatrix( nGoods, nCoef, 1, "Ex", qNames, pNames )
+   jacobian$hicks    <- createMatrix( nGoods, nCoef, 2, "Eh", qNames, pNames )
+   jacobian$marshall <- createMatrix( nGoods, nCoef, 2, "Em", qNames, pNames )
 
    share <- array( share )
 
@@ -46,16 +61,16 @@ aidsElaJacobian <- function( coef, share, price = NULL, formula = "AIDS" ) {
    bName <- paste( "beta", c( 1:nGoods ) )
    gName <- array( paste( "gamma", rep( 1:nGoods, nGoods ),
       rep( 1:nGoods, each = nGoods ) ), dim = c( nGoods, nGoods ) )
-   ehName <- array( paste( "Eh", rep( 1:nGoods, nGoods ),
-      rep( 1:nGoods, each = nGoods ) ), dim = c( nGoods, nGoods ) )
-   emName <- array( paste( "Em", rep( 1:nGoods, nGoods ),
-      rep( 1:nGoods, each = nGoods ) ), dim = c( nGoods, nGoods ) )
+   ehName <- array( paste( "Eh", rep( qNames, nGoods ),
+      rep( pNames, each = nGoods ) ), dim = c( nGoods, nGoods ) )
+   emName <- array( paste( "Em", rep( qNames, nGoods ),
+      rep( pNames, each = nGoods ) ), dim = c( nGoods, nGoods ) )
 
    if( formula == "AIDS" ) {
       price <- array( price )
       for( i in 1:nGoods ) {
          # expenditure elasticities
-         jacobian$exp[ paste( "Ex", i ), bName[ i ] ] <-
+         jacobian$exp[ paste( "Ex", qNames[ i ] ), bName[ i ] ] <-
             1 / share[ i ]
          for( j in 1:nGoods ) {
             # Hicksian price elasticities
