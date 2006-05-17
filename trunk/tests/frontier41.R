121150999014417f6cfcace38698645b26d3d8b1
suapm095 <unknown@noreply.com>
2006-05-17 08:12:54 +0000

    inst/front41/EG1.OUT, man/readFront41out.Rd, tests/frontier41.R(out.save): added output file that is provided with Frontier 4.1; added example for readFront41out using this file; added test for readFront41out using this file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@277 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/tests/frontier41.R
+++ trunk/tests/frontier41.R
@@ -1,4 +1,8 @@
 library( micEcon )
+
+## *****************************
+## Testing writeFront41in
+
 data( Coelli )
 Coelli$logOutput  <- log( Coelli$output )
 Coelli$logCapital <- log( Coelli$capital )
@@ -15,6 +19,10 @@ print( readLines( dtaFile ) )
 close( insFile )
 close( dtaFile )
 
+## *****************************
+## Testing readFront41out
 
-
-
+outFile <- system.file( "front41/EG1.OUT", package = "micEcon" )
+sfa <- readFront41out( outFile )
+print( sfa$mleResults )
+print( sfa$efficiency )
