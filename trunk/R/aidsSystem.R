7062dd365f8c4b1971c71760579edecb411133a9
suapm095 <unknown@noreply.com>
2006-06-22 15:44:58 +0000

    trunk/R/aidsSystem.R: extended function aidsSystem() that it can create the formulas of an AIDS systems with demand shifters?
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@288 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/aidsSystem.R
+++ trunk/R/aidsSystem.R
@@ -7,6 +7,12 @@ aidsSystem <- function( nGoods, nShifter = 0, LA = TRUE ) {
         system[[ i ]] <- paste( system[[ i ]], " + lp",
            as.character( j ), sep = "" )
       }
+      if( nShifter > 0 ) {
+         for( j in 1:nShifter ) {
+            system[[ i ]] <- paste( system[[ i ]], " + s",
+               as.character( j ), sep = "" )
+         }
+      }
       system[[ i ]] <- as.formula( system[[ i ]] )
     }
   }
