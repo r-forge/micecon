260d9a1d87e1ecdbf583ae50652189f1d6951e84
suapm095 <suapm095@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2006-06-23 12:48:18 +0000

    trunk/*/aidsCoef.R: simplified extraction of coefficients by using names instead of numbers; added possibility to extract coefficients of demand shifters
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@292 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/aidsCoef.R
+++ trunk/R/aidsCoef.R
@@ -1,27 +1,39 @@
 aidsCoef <- function( coef, nGoods, nShifter = 0, cov = NULL, df = 1,
       LA = TRUE, priceNames = NULL, shareNames = NULL ) {
    # nGoods <- -0.5 + ( 2.25 + nrow( array( coef ) ) )^0.5
+   nExogEq <- nGoods + 2 + nShifter
    if( LA ) {
-      M <- matrix( 0, nGoods * ( nGoods + 2 ), ( nGoods - 1 ) * ( nGoods + 2 ) )
+      M <- matrix( 0, nGoods * nExogEq, ( nGoods - 1 ) * nExogEq )
+      rownames( M ) <- .aidsCoefNamesAll( nGoods, nShifter )
+      colnames( M ) <- .aidsCoefNamesEst( nGoods, nShifter, hom = FALSE,
+         sym = FALSE )
+      aName <- paste( "alpha", c( 1:nGoods ) )
+      bName <- paste( "beta", c( 1:nGoods ) )
+      gName <- matrix( paste( "gamma", rep( 1:nGoods, nGoods ),
+         rep( 1:nGoods, each = nGoods ) ), nrow = nGoods, ncol = nGoods )
       for(i in 1:( nGoods - 1 ) ) {
-         M[ i,( i - 1 ) * ( nGoods + 2 ) + 1 ]   <-  1   # alpha[i]
-         M[ nGoods, ( i - 1 ) * ( nGoods + 2 ) + 1 ]   <- -1   # alpha[nGoods]
-         M[ nGoods + i, ( i - 1 ) * ( nGoods + 2 ) + 2] <-  1   # beta[i]
-         M[ nGoods + nGoods, ( i - 1 ) * ( nGoods + 2 ) + 2 ] <- -1
-            # beta[ nGoods ]
+         M[ aName[ i ], aName[ i ] ]   <-  1   # alpha[i]
+         M[ aName[ nGoods ], aName[ i ] ]   <- -1   # alpha[nGoods]
+         M[ bName[ i ], bName[ i ] ] <-  1   # beta[i]
+         M[ bName[ nGoods ], bName[ i ] ] <- -1 # beta[ nGoods ]
          for( j in 1:nGoods ) {
-            M[ 2 * nGoods + ( i - 1 ) * nGoods + j,
-               (i - 1 ) * ( nGoods + 2 ) + 2 + j ] <-  1   # gamma[i,j]
-            M[ 2 * nGoods + ( nGoods - 1 ) * nGoods + j,
-               ( i - 1 ) * ( nGoods + 2 ) + 2 + j ] <- -1   # gamma[nGoods,j]
+            M[ gName[ i, j ], gName[ i, j ] ] <-  1   # gamma[i,j]
+            M[ gName[ nGoods, j ], gName[ i, j ] ] <- -1   # gamma[nGoods,j]
+         }
+        if( nShifter > 0 ){
+            for( j in 1:nShifter ) {
+               M[ gName[ i, j ], gName[ i, j ] ] <-  1   # gamma[i,j]
+               M[ gName[ nGoods, j ], gName[ i, j ] ] <- -1   # gamma[nGoods,j]
+            }
          }
       }
       all     <- c( M %*% coef )
-      all[nGoods]  <- all[nGoods]+1
       names( all ) <- .aidsCoefNamesAll( nGoods, nShifter )
+      all[ aName[ nGoods ] ]  <- all[ aName[ nGoods ] ] + 1
       alpha   <- all[1:nGoods]
       beta    <- all[(nGoods+1):(2*nGoods)]
-      gamma   <- t(array(all[(2*nGoods+1):(nGoods*(nGoods+2))],c(nGoods,nGoods)))
+      gamma   <- matrix( all[(2*nGoods+1):(nGoods*(nGoods+2))],
+         nrow = nGoods, ncol = nGoods, byrow = TRUE )
       allcov <- NULL
       stat    <- NULL
       if(!is.null(cov)) {
