259737cf1d1131c73a921cabc16f5fd062aceeb8
otoomet <otoomet@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2006-07-02 13:55:22 +0000

    summary.tobit5.R: separated from tobit5.R activePar.Rd: added NObs.Rd: example works now activePar(): now has ... as second argument
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@299 95fa4d17-b1e8-0310-9d42-a43d67248e42

---
+++ trunk/R/summary.tobit5.R
@@ -0,0 +1,8 @@
+
+summary.tobit5 <- function(object, ...) {
+    opt <- summary(object$results)
+                                        # class maximisation
+    a <- list(opt=opt)
+    class(a) <- "summary.tobit5"
+    a
+}
