9f66be3ad143ae477f40ff78531b021bd09cad50
otoomet (Ott Toomet) <otoomet@gmail.com>
2006-12-02 17:05:06 +0000

    R/selection.R: worked on the concept and initial checks
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@326 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/selection.R
+++ trunk/R/selection.R
@@ -7,28 +7,57 @@ selection <- function(selection, outcome,
                       model=FALSE,
                       ...) {
    ## Heckman-style sample-selection models
-      ## --- the main program ---
+   ## selection:   formula
+   ##              LHS: must be convertable to two-level factor (e.g. 0-1, 1-2, "A"-"B")
+   ##              RHS: ordinary formula as in lm()
+   ## outcome:     formula, or a list of two formulas.
+   ##              If the outcome contains one formula, it is
+   ##              assumed that we observe the formula only if selection equals to the first level
+   ##              of selection (e.g. "0", "1" or "A" in the examples above).
+   ##              If the outcome contains two formulas, we assume that the first one is observed if
+   ##              selection equals to the first level, otherwise the second formula.
    ## First the consistency checks
-   if( class( selection ) != "formula" ) {
-      stop( "argument 'outcome' must be a formula" )
-   }
-   if( length( formula ) != 3 ) {
-      stop( "argument 'outcome' must be a 2-sided formula" )
-   }
-   if( "probit" %in% substr( all.vars( outcome ), 1, 6 ) ) {
-      stop( "argument 'outcome' may not include variable names",
-                  " starting with 'probit'" )
-   }
+   type <- 0
    if( class( selection ) != "formula" ) {
       stop( "argument 'selection' must be a formula" )
    }
    if( length( selection ) != 3 ) {
       stop( "argument 'selection' must be a 2-sided formula" )
    }
-   if( "probit" %in% substr( all.vars( selection ), 1, 6 ) ) {
-      stop( "argument 'selection' may not include a variable",
-                  " names starting with 'probit'" )
+                                        #
+   if("formula" %in% class( outcome )) {
+      if( length( outcome ) != 3 ) {
+         stop( "argument 'outcome' must be a 2-sided formula" )
+      }
+      type <- 2
    }
+   else if("list" %in% class(outcome)) {
+      if(length(outcome) == 1) {
+         outcome <- outcome[[1]]
+         type <- 2
+      }
+      else if(length(outcome) == 2) {
+         if("formula" %in% class(outcome[[1]])) {
+            if( length( outcome[[1]] ) != 3 ) {
+               stop( "argument 'outcome[[1]]' must be a 2-sided formula" )
+            }
+         }
+         else
+             stop( "argument 'outcome[[1]]' must be either a formula or a list of two formulas" )
+         if("formula" %in% class(outcome[[2]])) {
+            if( length( outcome[[2]] ) != 3 ) {
+               stop( "argument 'outcome[[2]]' must be a 2-sided formula" )
+            }
+         }
+         else
+             stop( "argument 'outcome[[2]]' must be either a formula or a list of two formulas" )
+         type <- 5
+      }
+      else
+          stop("argument 'outcome' must contain 1 or 2 components")
+   }
+   else
+       stop( "argument 'selection' must be either a formula or a list of two formulas" )
    probitEndogenous <- model.frame( selection, data = data )[ , 1 ]
    probitLevels <- levels( as.factor( probitEndogenous ) )
    if( length( probitLevels ) != 2 ) {
@@ -36,13 +65,15 @@ selection <- function(selection, outcome,
          " exactly two levels (e.g. FALSE and TRUE)" )
    }
    data$probitDummy <- probitEndogenous == probitLevels[ 2 ]
-   ## now check whether two-step method is needed: either for final estimate or
-   ## initial parameters
-   if(method == "2step" | is.null(init)) {
-      twoStep <- heckit(selection, outcome, data)
-      if(method == "2step") {
-         return(twoStep)
-      }
+   ## now check whether two-step method was requested
+   if(method == "2step") {
+      if(type == 2)
+          twoStep <- heckit2(selection, outcome, data)
+      else if(type == 5)
+          twoStep <- heckit5(selection, outcome, data)
+      else
+          stop("unknown type")
+      return(twoStep)
    }
    ## Now extract model frames etc
    ## YS (selection equation)
@@ -61,22 +92,41 @@ selection <- function(selection, outcome,
    XS <- model.matrix(mt1, mf1)
    YS <- model.response(mf1, "numeric")
    ## YO (outcome equation)
-   m <- match(c("outcome", "data", "subset", "weights", "na.action",
-                "offset"), names(mf), 0)
-   mf2 <- mf[c(1, m)]
-   mf2$drop.unused.levels <- TRUE
-   mf2[[1]] <- as.name("model.frame")
-   names(mf2)[2] <- "formula"
-   mf2 <- eval(mf2, parent.frame())
+   if(type == 2) {
+      m <- match(c("outcome", "data", "subset", "weights", "na.action",
+                   "offset"), names(mf), 0)
+      mf2 <- mf[c(1, m)]
+      mf2$drop.unused.levels <- TRUE
+      mf2[[1]] <- as.name("model.frame")
+      names(mf2)[2] <- "formula"
+      mf2 <- eval(mf2, parent.frame())
                                         # Note: if unobserved variables are
                                         # marked as NA, eval returns a
                                         # subframe of visible variables only.
                                         # We have to check it later
-   mt2 <- attr(mf2, "terms")
-   XO <- model.matrix(mt2, mf2)
-   YO <- model.response(mf2, "numeric")
+      mt2 <- attr(mf2, "terms")
+      XO <- model.matrix(mt2, mf2)
+      YO <- model.response(mf2, "numeric")
+      estimation <- tobit2.fit(YS, XS, YO, XO, init)
+   }
+   if(type == 5) {
+      m <- match(c("outcome", "data", "subset", "weights", "na.action",
+                   "offset"), names(mf), 0)
+      mf2 <- mf[c(1, m)]
+      mf2$drop.unused.levels <- TRUE
+      mf2[[1]] <- as.name("model.frame")
+      names(mf2)[2] <- "formula"
+      mf2 <- eval(mf2, parent.frame())
+                                        # Note: if unobserved variables are
+                                        # marked as NA, eval returns a
+                                        # subframe of visible variables only.
+                                        # We have to check it later
+      mt2 <- attr(mf2, "terms")
+      XO1 <- model.matrix(mt2, mf2)
+      YO1 <- model.response(mf2, "numeric")
+      estimation <- tobit5.fit(YS, XS, YO1, XO1, YO2, XO2, init)
+   }
    ## now fit the model
-   estimation <- tobit2.fit(YS, XS, YO, XO, init)
    result <- c(estimation,
                twoStep=list(twoStep),
                NParam=NParam,
