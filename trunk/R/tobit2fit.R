975ae6aa1e1a637d62915cc819c7eb657f6dd646
otoomet <otoomet@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2007-01-19 18:04:07 +0000

    selection with tobit2 now runs, although summary does not work and I haven't tested it.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@335 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/tobit2fit.R
+++ trunk/R/tobit2fit.R
@@ -31,15 +31,15 @@ tobit2fit <- function(YS, XS, YO, XO, init,
       if(sigma < 0) return(NA)
       rho <- beta[irho]
       if( ( rho < -1) || ( rho > 1)) return(NA)
-      Z1.g <- Z1 %*% g
-      Z2.g <- Z2 %*% g
-      X2.b <- X2 %*% b
-      u2 <- Y21 - X2.b
+      XS0.g <- XS0 %*% g
+      XS1.g <- XS1 %*% g
+      XO1.b <- XO1 %*% b
+      u2 <- YO1 - XO1.b
       r <- sqrt( 1 - rho^2)
-      B <- (Z2.g + rho/sigma*u2)/r
-      l1 <- sum(pnorm(-Z1.g, log.p=TRUE))
+      B <- (XS1.g + rho/sigma*u2)/r
+      l1 <- sum(pnorm(-XS0.g, log.p=TRUE))
                                         # loglik related with unobserved cases ...
-      l2 <- -N2/2*log(2*pi) - N2*log(sigma) +
+      l2 <- -N1/2*log(2*pi) - N1*log(sigma) +
           sum(pnorm(B, log.p=TRUE) - 0.5*(u2/sigma)^2)
                                         # ... and observed cases
       loglik <- l1 + l2
@@ -51,19 +51,19 @@ tobit2fit <- function(YS, XS, YO, XO, init,
        if(sigma < 0) return(NA)
        rho <- beta[irho]
        if( ( rho < -1) || ( rho > 1)) return(NA)
-       Z1.g <- Z1 %*% g
-       Z2.g <- Z2 %*% g
-       X2.b <- X2 %*% b
-       u2 <- Y21 - X2.b
+       XS0.g <- XS0 %*% g
+       XS1.g <- XS1 %*% g
+       XO1.b <- XO1 %*% b
+       u2 <- YO1 - XO1.b
        r <- sqrt( 1 - rho^2)
-       B <- (Z2.g + rho/sigma*u2)/r
+       B <- (XS1.g + rho/sigma*u2)/r
        lambdaB <- dnorm(B)/pnorm(B)
        gradient <- numeric(NParam)
-       gradient[igamma] <- t(Z1) %*% (-dnorm(-Z1.g)/pnorm(-Z1.g)) +
-           (t(Z2) %*% lambdaB)/r
-       gradient[ibeta] <- t(X2) %*% (u2/sigma^2 - lambdaB*rho/sigma/r)
-       gradient[isigma] <- sum(u2^2/sigma^3 - lambdaB*rho*u2/sigma^2/r) - N2/sigma
-       gradient[irho] <- sum(lambdaB*(u2/sigma + rho*Z2.g))/r^3
+       gradient[igamma] <- t(XS0) %*% (-dnorm(-XS0.g)/pnorm(-XS0.g)) +
+           (t(XS1) %*% lambdaB)/r
+       gradient[ibeta] <- t(XO1) %*% (u2/sigma^2 - lambdaB*rho/sigma/r)
+       gradient[isigma] <- sum(u2^2/sigma^3 - lambdaB*rho*u2/sigma^2/r) - N1/sigma
+       gradient[irho] <- sum(lambdaB*(u2/sigma + rho*XS1.g))/r^3
        gradient
     }
     hesslik <- function(beta) {
@@ -73,15 +73,15 @@ tobit2fit <- function(YS, XS, YO, XO, init,
        if(sigma < 0) return(NA)
        rho <- beta[irho]
        if( ( rho < -1) || ( rho > 1)) return(NA)
-       Z1.g <- as.vector(Z1 %*% g)
-       Z2.g <- as.vector(Z2 %*% g)
-       X2.b <- as.vector(X2 %*% b)
-       u2 <- Y21 - X2.b
+       XS0.g <- as.vector(XS0 %*% g)
+       XS1.g <- as.vector(XS1 %*% g)
+       XO1.b <- as.vector(XO1 %*% b)
+       u2 <- YO1 - XO1.b
        r <- sqrt( 1 - rho^2)
-       B <- (Z2.g + rho/sigma*u2)/r
+       B <- (XS1.g + rho/sigma*u2)/r
        lambdaB <- dnorm(B)/pnorm(B)
-       fZ1.g <- dnorm(-Z1.g)
-       FZ1.g <- pnorm(-Z1.g)
+       fXS0.g <- dnorm(-XS0.g)
+       FXS0.g <- pnorm(-XS0.g)
                                         # the previous code
                                         #       C <- ifelse(B > -25,
                                         #                   -(pnorm(B)*dnorm(B)*B + dnorm(B)^2)/pnorm(B)^2,
@@ -94,36 +94,36 @@ tobit2fit <- function(YS, XS, YO, XO, init,
                                         # This is a hack in order to avoid numerical problems.  How to do
                                         # it better?  How to prove the limit value?
        hess <- matrix(0, NParam, NParam)
-       a <- (-fZ1.g*FZ1.g*Z1.g + fZ1.g^2)/FZ1.g^2
-       hess[igamma,igamma] <- -t(Z1) %*% (Z1*a) + t(Z2) %*% (Z2*C)/r^2
-       hess[igamma,ibeta] <- -t(Z2) %*% (X2*C)*rho/r^2/sigma
+       a <- (-fXS0.g*FXS0.g*XS0.g + fXS0.g^2)/FXS0.g^2
+       hess[igamma,igamma] <- -t(XS0) %*% (XS0*a) + t(XS1) %*% (XS1*C)/r^2
+       hess[igamma,ibeta] <- -t(XS1) %*% (XO1*C)*rho/r^2/sigma
        hess[ibeta,igamma] <- t(hess[igamma,ibeta])
-       hess[igamma,isigma] <- -rho/sigma^2/r^2*t(Z2) %*% (C*u2)
+       hess[igamma,isigma] <- -rho/sigma^2/r^2*t(XS1) %*% (C*u2)
        hess[isigma,igamma] <- t(hess[igamma,isigma])
-       hess[igamma,irho] <- t(Z2) %*%
-           (C*(u2/sigma + rho*Z2.g)/r^4 + lambdaB*rho/r^3)
+       hess[igamma,irho] <- t(XS1) %*%
+           (C*(u2/sigma + rho*XS1.g)/r^4 + lambdaB*rho/r^3)
        hess[irho,igamma] <- t(hess[igamma,irho])
-       hess[ibeta,ibeta] <- t(X2) %*%
-           (X2 * ((rho/r)^2*C - 1))/sigma^2
-       hess[ibeta,isigma] <- t(X2) %*%
+       hess[ibeta,ibeta] <- t(XO1) %*%
+           (XO1 * ((rho/r)^2*C - 1))/sigma^2
+       hess[ibeta,isigma] <- t(XO1) %*%
            (C*rho^2/sigma^3*u2/r^2 +
             rho/sigma^2*lambdaB/r - 2*u2/sigma^3)
        hess[isigma,ibeta] <- t(hess[ibeta,isigma])
-       hess[ibeta,irho] <- t(X2) %*%
-           (-C*(u2/sigma + rho*Z2.g)/r^4*rho -
+       hess[ibeta,irho] <- t(XO1) %*%
+           (-C*(u2/sigma + rho*XS1.g)/r^4*rho -
             lambdaB/r^3)/sigma
        hess[irho,ibeta] <- t(hess[ibeta,irho])
        hess[isigma,isigma] <- sum(
                                   -3*u2*u2/sigma^4
                                   +2*lambdaB* u2/r *rho/sigma^3
                                   +rho^2/sigma^4 *u2*u2/r^2 *C) +
-                                      N2/sigma^2
+                                      N1/sigma^2
        hess[isigma,irho] <- hess[irho,isigma] <-
-           -sum((C*rho*(u2/sigma + rho*Z2.g)/r + lambdaB)*
+           -sum((C*rho*(u2/sigma + rho*XS1.g)/r + lambdaB)*
                 u2/sigma^2)/r^3
        hess[irho,irho] <-
-           sum(C*((u2/sigma + rho*Z2.g)/r^3)^2 +
-               lambdaB*(Z2.g*(1 + 2*rho^2) + 3*rho*u2/sigma) / r^5 )
+           sum(C*((u2/sigma + rho*XS1.g)/r^3)^2 +
+               lambdaB*(XS1.g*(1 + 2*rho^2) + 3*rho*u2/sigma) / r^5 )
        return( hess )
     }
     ## ---------------
@@ -137,28 +137,30 @@ tobit2fit <- function(YS, XS, YO, XO, init,
                                         # Total # of parameters
     Nobs <- length( YS)
     NO <- length( YS[YS > 0])
-    ## indices in for the parameter vector
-    igamma <- 1:NXS
-    ibeta <- seq(tail(igamma, 1)+1, length=NXO)
-    isigma <- tail(ibeta, 1) + 1
-    irho <- tail(isigma, 1) + 1
-    ## split data by selection
-    YO <- YO[YS==0]
-    XO <- XO[YS==0,,drop=FALSE]
-    XS1 <- XS[YS==0,,drop=FALSE]
-    XS2 <- XS[YS==1,,drop=FALSE]
-    if(print.level > 0) {
-        cat( "Yo observed:", NO, "times; not observed:", NObs - NO, "times\n")
-    }
-    if( print.level > 0) {
-        cat( "Initial values:\n")
-        cat("beta1\n")
-        print(init[ibeta])
-        cat("sigma1\n")
-        print(init[isigma])
-        cat("rho1\n")
-        print(init[irho])
-    }
+   NParam <- NXS + NXO + 2
+   ## output, if asked for it
+   if( print.level > 0) {
+      cat( "Yo observed:", NO, "times; not observed:", NObs - NO, "times\n")
+      cat( "Initial values:\n")
+      cat("beta1\n")
+      print(init[ibeta])
+      cat("sigma1\n")
+      print(init[isigma])
+      cat("rho1\n")
+      print(init[irho])
+   }
+   ## pre-calculate a few values:
+   XS0 <- XS[YS==0,,drop=FALSE]
+   XS1 <- XS[YS==1,,drop=FALSE]
+   YO1 <- YO[YS==1]
+   XO1 <- XO[YS==1,,drop=FALSE]
+   N0 <- sum(YS==0)
+   N1 <- sum(YS==1)
+   igamma <- 1:NXS
+   ibeta <- seq(tail(igamma, 1)+1, length=NXO)
+   isigma <- tail(ibeta, 1) + 1
+   irho <- tail(isigma, 1) + 1
+   ## estimate
    estimation <- maxLik(loglik, grad=gradlik, hess=hesslik, theta=init,
                      print.level=print.level, ...)
     result <- c(estimation,
