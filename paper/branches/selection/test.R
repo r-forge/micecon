6b297c1d27428109be9a956d351a52031bdc9f8b
otoomet <otoomet@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2007-03-01 22:33:26 +0000

    example of a tobit-problem, where on should prefer 2step instead of ml
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@844 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- paper/branches/selection/test.R
+++ paper/branches/selection/test.R
@@ -13,3 +13,12 @@ tst5 <- function(N=500, print.level=0) {
    a <- selection(ys~xs, list(yo1 ~ xo1, yo2 ~ xo2), print.level=print.level)
    invisible(a)
 }
+
+tstOne <- function(N=1000, print.level=0) {
+   x <- runif(N, -2, 2)
+   e <- rnorm(N)
+   z <- x + e > 0
+   y <- (x - e)*z
+   a <- selection(z ~ x, y ~ x, method="2step")
+   print(summary(a))
+}
