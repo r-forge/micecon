39884f66528da266b3fe1aa99a6cca1b15fee6e0
otoomet (Ott Toomet) <otoomet@gmail.com>
2007-03-06 15:11:56 +0000

    Implementation section improved Written examples for exclusion restriction and identification at infinity. a figure on observation probability added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@845 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- paper/branches/selection/test.R
+++ paper/branches/selection/test.R
@@ -1,3 +1,38 @@
+tst2 <- function(N=1000, rho=-0.7, print.level=0) {
+   library(mvtnorm)
+   vc <- diag(2)
+   vc[2,1] <- vc[1,2] <- -0.7
+   eps <- rmvnorm(N, rep(0, 2), vc)
+   xs <- runif(N)
+   ys <- xs + eps[,1] > 0
+   xo <- runif(N)
+   yo <- (xo + eps[,2])*(ys > 0)
+   a <- selection(ys~xs, yo ~xo, print.level=print.level)
+   invisible(a)
+}
+
+tstA <- function() {
+   library(mvtnorm)
+   eps <- rmvnorm(500, c(0,0), matrix(c(1,-0.7,-0.7,1), 2, 2))
+   xs <- runif(500)
+   ys <- xs + eps[,1] > 0
+   yo <- (xs + eps[,2])*(ys > 0)
+   print(summary(selection(ys ~ xs, yo ~ xs)))
+}
+
+tstB <- function() {
+   library(mvtnorm)
+   eps <- rmvnorm(500, c(0,0), matrix(c(1,-0.7,-0.7,1), 2, 2))
+   xs <- runif(500, -5, 5)
+   ys <- xs + eps[,1] > 0
+   yo <- (xs + eps[,2])*(ys > 0)
+   pdf(file="pnorm.pdf", width=6, height=4)
+   curve(pnorm, -5, 5,
+         ylab=expression(1 - Phi(-x^S)), xlab=expression(x^S))
+   dev.off()
+   print(summary(selection(ys ~ xs, yo ~ xs)))
+}
+
 tst5 <- function(N=500, print.level=0) {
    library(mvtnorm)
    vc <- diag(3)
