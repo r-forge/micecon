b88bf3ab7b19a597b4d867a49b214ba4fb640bfe
otoomet <otoomet@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2007-04-06 18:58:55 +0000

    summary.heckit5 implemented coef.heckit5 implemented vcov.heckit5 implemented summary.heckit? output much closer to that of summary.tobit? part=full/outcome moved to print.summary, not summary invMillsRatio is not in the coefficients any more (can be queried by the corresponding linear model)
    documentation not implemented
    very basic testing only
    
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@364 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/coef.heckit.R
+++ trunk/R/coef.heckit.R
@@ -9,3 +9,16 @@ coef.heckit <- function(object, part="outcome", ...) {
        stop("'part' must be either 'outcome' or 'full'")
    b
 }
+
+coef.heckit5 <- function(object, part="outcome", ...) {
+   if(part=="outcome") {
+      b <- object$coefficients[c(object$param$index$betaO1, object$param$index$invMillsRatio1,
+                                 object$param$index$betaO2, object$param$index$invMillsRatio2)]
+   }
+   else if(part=="full") {
+      b <- object$coefficients
+   }
+   else
+       stop("'part' must be either 'outcome' or 'full'")
+   b
+}
