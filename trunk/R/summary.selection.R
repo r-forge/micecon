7d6b52076daec3d8fd0605ac843586e1fac22455
suapm095 <unknown@noreply.com>
2007-04-23 11:26:24 +0000

    merged (print.)summary.tobit and (print.)summary.heckit into (print.)summary.selection (changes are not visible to the user); renamed documentation of (print.)summary.heckit to (print.)summary.selection and modified it accordingly
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@394 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/summary.selection.R
+++ trunk/R/summary.selection.R
@@ -4,22 +4,134 @@ summary.selection <- function(object, ...) {
    ## ...         additional arguments for "summary.tobit" and "summary.heckit"
 
    if( object$method == "ml" ) {
-      result <- summary.tobit( object, ... )
+      s <- summary.maxLik(object, ...)
    } else if( object$method == "2step" )  {
-      result <- summary.heckit( object, ... )
+      s <- list()  # list for results that will be returned
+
+      RSq <- function(model, intercept) {
+         ## Calculate r-squared.  Note that the way lm() finds R2 is a bit naive -- it checks for intercept
+         ## in the formula, but not whether the intercept is present in any of the data vectors (or matrices)
+         if(class(model) == "lm") {
+            y <- model.response(model.frame(model))
+            if(intercept) {
+               R2 <- 1 - sum(residuals(model)^2)/sum((y - mean(y))^2)
+               R2adj <- 1 - (1 - R2)*(NObs(model) - 1)/(NObs(model) - NParam(model))
+            }
+            else {
+               R2 <- 1 - sum(residuals(model)^2)/sum(y^2)
+               R2adj <- 1 - (1 - R2)*(NObs(model))/(NObs(model) - NParam(model))
+            }
+         }
+         else {
+            R2 <- model$eq[[ 1 ]]$r2
+            R2adj <- model$eq[[ 1 ]]$adjr2
+         }
+         c(R2, R2adj)
+      }
+      if( object$tobitType == 2 ) {
+         r <- RSq( object$lm, object$param$oIntercept )
+         R2 <- r[1]
+         R2adj <- r[2]
+         s$rSquared <- list(R2=R2, R2adj=R2adj)
+      } else {
+         r <- RSq(object$lm1, object$param$oIntercept1)
+         R21 <- r[1]
+         R2adj1 <- r[2]
+         r <- RSq(object$lm2, object$param$oIntercept2)
+         R22 <- r[1]
+         R2adj2 <- r[2]
+         iBetaS <- object$param$index$betaS
+         s$rSquared <- list(R21=R21, R2adj1=R2adj1, R22=R22, R2adj2=R2adj2)
+      }
+
+      stdd <- sqrt(diag(vcov(object, part="full")))
+      estcoef <- coef( object, part="full" )
+      names( estcoef ) <- sub( "^[SO][12]?:", "", names( estcoef ) )
+      s$estimate <- coefTable( estcoef, stdd, object$param$df)
    }
-   result$method <- object$method
-   class( result ) <- c( "summary.selection", class( result ) )
-   return( result )
+   s$param    <- object$param
+   s$tobitType <- object$tobitType
+   s$method <- object$method
+   class( s ) <- c( "summary.selection", class( s ) )
+   return( s )
 }
 
-print.summary.selection <- function(x, ...) {
+print.summary.selection <- function(x,
+                                 digits=max(3, getOption("digits") - 3),
+                                 part="full",
+                                 ... ) {
 
    cat("--------------------------------------------\n")
+   cat("Tobit", x$tobitType, "model" )
+   if( x$tobitType == 2 ) {
+      cat( " (sample selection model)\n" )
+   } else {
+      cat( " (switching regression model)\n" )
+   }
    if( x$method == "ml" ) {
-      print.summary.tobit( x, ... )
+      cat( "Maximum Likelihood estimation\n" )
+      cat(x$type, ", ", x$iterations, " iterations\n", sep="")
+      cat("Return code ", x$code, ": ", x$message, "\n", sep="")
+      if(!is.null(x$estimate)) {
+         cat("Log-Likelihood:", loglikValue(x), "\n")
+      }
    } else if( x$method == "2step" ) {
-      print.summary.heckit( x, ... )
+      cat( "2-step Heckman / heckit estimation\n" )
+   }
+   if(!is.null(x$estimate)) {
+      cat( x$param$NObs, "observations" )
+      if( x$tobitType == 2 ) {
+         cat( " (", x$param$N0, " censored and ", x$param$N1, " observed)",
+            sep = "" )
+      } else {
+         cat( " (", x$param$N1, " selection 1 and ",
+            x$param$N2, " selection 2)", sep = "" )
+      }
+      cat( " and", x$param$NParam, "free parameters" )
+      cat( " (df = ", x$param$df, ")\n", sep="")
+      if(part == "full") {
+         cat("Probit selection equation:\n")
+         printCoefmat( x$estimate[ x$param$index$betaS, ],
+            signif.legend = FALSE )
+      }
+      if( x$tobitType == 2 ) {
+         cat("Outcome equation:\n")
+         printCoefmat( x$estimate[ x$param$index$betaO, ],
+            signif.legend = ( part != "full" ) )
+         if( x$method == "2step" ) {
+            cat("Multiple R-Squared:", round(x$rSquared$R2, digits),
+               ",\tAdjusted R-Squared:", round(x$rSquared$R2adj, digits),
+               "\n", sep="")
+         }
+      } else if( x$tobitType == 5 ) {
+         cat("Outcome equation 1:\n")
+         printCoefmat( x$estimate[ x$param$index$betaO1, ],
+            signif.legend = FALSE )
+         if( x$method == "2step" ) {
+            cat("Multiple R-Squared:", round(x$rSquared$R21, digits),
+               ",\tAdjusted R-Squared:", round(x$rSquared$R2adj1, digits),
+               "\n", sep="")
+         }
+         cat("Outcome equation 2:\n")
+         printCoefmat( x$estimate[ x$param$index$betaO2, ],
+            signif.legend = ( part != "full" ) )
+         if( x$method == "2step" ) {
+            cat("Multiple R-Squared:", round(x$rSquared$R22, digits),
+               ",\tAdjusted R-Squared:", round(x$rSquared$R2adj2, digits),
+            "\n", sep="")
+         }
+      }
+      if(part=="full") {
+         if( x$tobitType == 2 ) {
+            i <- c(x$param$index$Mills, x$param$index$sigma, x$param$index$rho)
+         } else if( x$tobitType == 5 ) {
+            i <- c( x$param$index$Mills1, x$param$index$Mills2,
+               x$param$index$sigma1, x$param$index$sigma2,
+               x$param$index$rho1, x$param$index$rho2 )
+         }
+         cat("Error terms:\n")
+         printCoefmat(x$estimate[i,])
+      }
    }
    cat("--------------------------------------------\n")
    invisible( x )
