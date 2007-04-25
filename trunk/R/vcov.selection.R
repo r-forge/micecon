b93054dc364d9fa60a22bdca86831035eb5f6300
suapm095 <suapm095@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2007-04-25 20:42:24 +0000

    kleinen aber wichtigen Tippfehler korrigiert
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@404 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/vcov.selection.R
+++ trunk/R/vcov.selection.R
@@ -5,7 +5,7 @@ vcov.selection <- function( object, part = "full", ... ) {
    }
 
    if( object$method == "ml" ){
-      result <- vcov.maxlik( object, ... )
+      result <- vcov.maxLik( object, ... )
    } else if( object$method == "2step" ) {
       result <- object$vcov
    }
