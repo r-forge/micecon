e01829d26147c706bb0d9082aee639346255724d
suapm095 <unknown@noreply.com>
2007-04-25 14:36:04 +0000

    extracted vcov method for selection models into a separate file (vcov.selection.R, from vcov-methods.R) and also extracted its documentation into a separate file (vcov.selection.Rd, from vcov-methods.Rd); a few adjustments and corrections were necessary
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@399 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/vcov-methods.R
+++ trunk/R/vcov-methods.R
@@ -6,48 +6,6 @@ vcov.probit <- function(object, ...) {
   return( result )
 }
 
-## heckit
-vcov.selection <- function( object, ... ) {
-   if( object$method == "ml" ){
-      result <- vcov.maxlik( object, ... )
-   } else if( object$method == "2step" ) {
-      if( object$tobitType == 2 ) {
-         result <- vcov.heckit( object, ... )
-      } else if( object$tobitType == 5 ) {
-         result <- vcov.heckit5( object, ... )
-      }
-   }
-   return( result )
-}
-
-## heckit
-vcov.heckit <- function(object, part="outcome", ...) {
-   if(part=="outcome") {
-      i <- c(object$param$index$betaO, object$param$index$Mills)
-      vc <- object$vcov[i,i]
-   }
-   else if(part=="full") {
-      vc <- object$vcov
-   }
-   else
-       stop("'part' must be either 'outcome' or 'full'")
-   vc
-  }
-
-## heckit 5
-vcov.heckit5 <- function(object, part="outcome", ...) {
-   if(part=="outcome") {
-      i <- c(object$param$index$betaO1, object$param$index$invMillsRatio1,
-             object$param$index$betaO2, object$param$index$invMillsRatio2)
-      vc <- object$vcov[i,i]
-   }
-   else if(part=="full") {
-      vc <- object$vcov
-   }
-   else
-       stop("'part' must be either 'outcome' or 'full'")
-   vc
-  }
 
 ## maxLik
 vcov.maxLik <- function(object, ...) {
