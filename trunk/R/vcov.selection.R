650ca53e422b00c227e027ad81cf85277c3bda56
suapm095 <suapm095@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2007-04-25 19:12:30 +0000

    merged vcov.heckit and vcov.heckit5 into vcov.selection
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@401 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/vcov.selection.R
+++ trunk/R/vcov.selection.R
@@ -1,42 +1,25 @@
-## heckit
 vcov.selection <- function( object, part = "full", ... ) {
+
+   if( !( part %in% c( "full", "outcome" ) ) ) {
+      stop( "argument 'part' must be either 'full' or 'outcome'" )
+   }
+
    if( object$method == "ml" ){
       result <- vcov.maxlik( object, ... )
    } else if( object$method == "2step" ) {
-      if( object$tobitType == 2 ) {
-         result <- vcov.heckit( object, part = part, ... )
-      } else if( object$tobitType == 5 ) {
-         result <- vcov.heckit5( object, part = part, ... )
+      result <- object$vcov
+   }
+
+   if( part == "outcome" ) {
+      if( object$tobitType == 2) {
+         i <- c( object$param$index$betaO, object$param$index$Mills )
+         result <- result[ i, i ]
+      } else if( object$tobitType == 5) {
+         i <- c(object$param$index$betaO1, object$param$index$Mills1,
+            object$param$index$betaO2, object$param$index$Mills2 )
+         result <- result[ i, i ]
       }
    }
+
    return( result )
-}
-
-## heckit
-vcov.heckit <- function(object, part="outcome", ...) {
-   if(part=="outcome") {
-      i <- c(object$param$index$betaO, object$param$index$Mills)
-      vc <- object$vcov[i,i]
-   }
-   else if(part=="full") {
-      vc <- object$vcov
-   }
-   else
-       stop("'part' must be either 'outcome' or 'full'")
-   vc
-  }
-
-## heckit 5
-vcov.heckit5 <- function(object, part="outcome", ...) {
-   if(part=="outcome") {
-      i <- c(object$param$index$betaO1, object$param$index$invMillsRatio1,
-             object$param$index$betaO2, object$param$index$invMillsRatio2)
-      vc <- object$vcov[i,i]
-   }
-   else if(part=="full") {
-      vc <- object$vcov
-   }
-   else
-       stop("'part' must be either 'outcome' or 'full'")
-   vc
 }
