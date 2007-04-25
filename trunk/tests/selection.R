cbff632566188029abf99e829e07c6b50d6ad059
suapm095 <suapm095@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2007-04-25 20:59:55 +0000

    added tests for print.selection, coef.selection, coef.summary.selection, and vcov.selection
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@406 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/tests/selection.R
+++ trunk/tests/selection.R
@@ -22,10 +22,26 @@ xo1[sample(N, NNA)] <- NA
 xo2[sample(N, NNA)] <- NA
 yo1[sample(N, NNA)] <- NA
 yo2[sample(N, NNA)] <- NA
-a <- selection(ys~xs, list(yo1 ~ xo1, yo2 ~ xo2), method="2step")
-print(summary(a))
-a <- selection(ys~xs, list(yo1 ~ xo1, yo2 ~ xo2), method="ml")
-print(summary(a))
+testTobit5TwoStep <- selection(ys~xs, list(yo1 ~ xo1, yo2 ~ xo2), method="2step")
+print( testTobit5TwoStep )
+print( summary( testTobit5TwoStep ) )
+print( coef( testTobit5TwoStep ), digits = 5 )
+print( coef( testTobit5TwoStep, part = "outcome" ), digits = 5 )
+print( coef( summary( testTobit5TwoStep ) ), digits = 5 )
+print( coef( summary( testTobit5TwoStep ), part = "outcome" ), digits = 5 )
+print( vcov( testTobit5TwoStep ), digits = 5 )
+print( vcov( testTobit5TwoStep, part = "outcome" ), digits = 5 )
+
+testTobit5Ml <- selection(ys~xs, list(yo1 ~ xo1, yo2 ~ xo2), method="ml")
+print( testTobit5Ml )
+print( summary( testTobit5Ml ) )
+print( coef( testTobit5Ml ), digits = 5 )
+print( coef( testTobit5Ml, part = "outcome" ), digits = 5 )
+print( coef( summary( testTobit5Ml ) ), digits = 5 )
+print( coef( summary( testTobit5Ml ), part = "outcome" ), digits = 5 )
+print( vcov( testTobit5Ml ), digits = 5 )
+print( vcov( testTobit5Ml, part = "outcome" ), digits = 5 )
+
 ## ------- Tobit-2 exmple -----------
 vc <- diag(2)
 vc[2,1] <- vc[1,2] <- -0.7
@@ -38,8 +54,22 @@ xs[sample(N, NNA)] <- NA
 ys[sample(N, NNA)] <- NA
 xo[sample(N, NNA)] <- NA
 yo[sample(N, NNA)] <- NA
-a <- selection(ys~xs, yo ~xo, method="2step")
-print(summary(a))
-a <- selection(ys~xs, yo ~xo, method="ml")
-print(summary(a))
+testTobit2TwoStep <- selection(ys~xs, yo ~xo, method="2step")
+print( testTobit2TwoStep )
+print( summary( testTobit2TwoStep ) )
+print( coef( testTobit2TwoStep ), digits = 5 )
+print( coef( testTobit2TwoStep, part = "outcome" ), digits = 5 )
+print( coef( summary( testTobit2TwoStep ) ), digits = 5 )
+print( coef( summary( testTobit2TwoStep ), part = "outcome" ), digits = 5 )
+print( vcov( testTobit2TwoStep ), digits = 5 )
+print( vcov( testTobit2TwoStep, part = "outcome" ), digits = 5 )
 
+testTobit2Ml <- selection(ys~xs, yo ~xo, method="ml")
+print( testTobit2Ml )
+print( summary( testTobit2Ml ) )
+print( coef( testTobit2Ml ), digits = 5 )
+print( coef( testTobit2Ml, part = "outcome" ), digits = 5 )
+print( coef( summary( testTobit2Ml ) ), digits = 5 )
+print( coef( summary( testTobit2Ml ), part = "outcome" ), digits = 5 )
+print( vcov( testTobit2Ml ), digits = 5 )
+print( vcov( testTobit2Ml, part = "outcome" ), digits = 5 )
