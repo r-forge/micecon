f0275748b5f8361d7d0defab5a241df8316c5828
suapm095 <unknown@noreply.com>
2007-04-29 17:26:30 +0000

    aidsEst does not calculate demand elasticities any more; this is now done by the new function summary.aidsEst
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@417 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/print.aidsEst.R
+++ trunk/R/print.aidsEst.R
@@ -44,17 +44,5 @@ print.aidsEst <- function( x, ... ) {
       cat( "delta\n" )
       print( x$coef$delta )
    }
-
-   cat( "\nDemand Elasticities " )
-   if( x$ela$formula == "Ch" ) {
-      cat( "(Formula of Chalfant / Goddard)\n" )
-   } else if( x$ela$formula == "AIDS" ) {
-      cat( "(original AIDS formula)\n" )
-   }
-   cat( "Expenditure Elasticities\n" )
-   print( x$ela$exp )
-   cat( "\nMarshallian (uncompensated) Price Elasticities\n" )
-   print( x$ela$marshall )
-   cat( "\nHicksian (compensated) Price Elasticities\n" )
-   print( x$ela$hicks )
+   invisible( x )
 }
