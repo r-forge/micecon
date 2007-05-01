373170891b35ac6e6012d6185dd192623b4ee588
suapm095 <suapm095@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2007-05-01 12:37:48 +0000

    added summary.aidsEst and print.summary.aidsEst; added .aidsEstMethod to produce string that explains the estimation method for print.aidsEst and print.summary.aidsEst (the output of print.aidsEst does not change); added print ( summary( aidsEst( ))) to /tests/aids.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@444 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/print.aidsEst.R
+++ trunk/R/print.aidsEst.R
@@ -2,37 +2,7 @@ print.aidsEst <- function( x, ... ) {
    cat( "\nDemand analysis with the Almost Ideal " )
    cat( "Demand System (AIDS)\n" )
    cat( "Estimation Method: " )
-   if( substr( x$method, 1, 2 ) == "LA" ) {
-      cat( "Linear Approximation (LA) with " )
-      if( x$px == "S" ) {
-         cat( "Stone Index (S)\n" )
-      } else if( x$px == "SL" ) {
-         cat( "lagged Stone Index (SL)\n" )
-      } else if( x$px == "P" ) {
-         cat( "Paasche Index (P)\n" )
-      } else if( x$px == "L" ) {
-         cat( "Laspeyres Index (L)\n" )
-      } else if( x$px == "T" ) {
-         cat( "Tornqvist Index (T)\n" )
-      } else {
-         cat( "unknown price index\n" )
-      }
-   } else if( substr( x$method, 1, 2 ) %in% c( "MK", "IL" ) ) {
-      cat( "'Iterated Linear Least Squares Estimator' (IL) starting with " )
-      if( x$px == "S" ) {
-         cat( "Stone Index (S)\n" )
-      } else if( x$px == "SL" ) {
-         cat( "lagged Stone Index (SL)\n" )
-      } else if( x$px == "P" ) {
-         cat( "Paasche Index (P)\n" )
-      } else if( x$px == "L" ) {
-         cat( "Laspeyres Index (L)\n" )
-      } else if( x$px == "T" ) {
-         cat( "Tornqvist Index (T)\n" )
-      } else {
-         cat( "unknown price index\n" )
-      }
-   }
+   cat( .aidsEstMethod( x$method, x$px ) )
    cat( "Estimated Coefficients:\n" )
    cat( "alpha\n" )
    print( x$coef$alpha )
