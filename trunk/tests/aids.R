ffcbfb058d963718a86b90c0de0448fc3b2b731f
suapm095 <unknown@noreply.com>
2007-05-01 12:37:48 +0000

    added summary.aidsEst and print.summary.aidsEst; added .aidsEstMethod to produce string that explains the estimation method for print.aidsEst and print.summary.aidsEst (the output of print.aidsEst does not change); added print ( summary( aidsEst( ))) to /tests/aids.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@444 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/tests/aids.R
+++ trunk/tests/aids.R
@@ -14,12 +14,14 @@ cat( paste( "\nRepeating the demand analysis of Blanciforti, Green",
 estResultLA <- aidsEst( pNames, wNames, "xFood",
    data = Blanciforti86[ set, ], method = "LA:SL" )
 print( estResultLA )
+print( summary( estResultLA ) )
 print( elas( estResultLA, formula = "Ch", quantNames = wNames ) )
 # imposing restrictions via TX
 estResultLATX <- aidsEst( pNames, wNames, "xFood",
    data = Blanciforti86[ set, ], method = "LA:SL",
    TX = TRUE )
 print( estResultLATX )
+print( summary( estResultLATX ) )
 print( elas( estResultLATX, formula = "Ch", quantNames = wNames ) )
 estResultLATX$call <- NULL
 estResultLATX$est$bt <- NULL
@@ -37,12 +39,14 @@ print( all.equal( estResultLA, estResultLATX ) )
 estResultLAhom <- aidsEst( pNames, wNames, "xFood", sym = FALSE,
    data = Blanciforti86[ set, ], method = "LA:SL" )
 print( estResultLAhom )
+print( summary( estResultLAhom ) )
 print( elas( estResultLAhom, formula = "Ch", quantNames = wNames ) )
 # imposing restrictions via TX
 estResultLAhomTX <- aidsEst( pNames, wNames, "xFood", sym = FALSE,
    data = Blanciforti86[ set, ], method = "LA:SL",
    TX = TRUE )
 print( estResultLAhomTX )
+print( summary( estResultLAhomTX ) )
 print( elas( estResultLAhomTX, formula = "Ch", quantNames = wNames ) )
 estResultLAhomTX$call <- NULL
 estResultLAhomTX$est$bt <- NULL
@@ -60,12 +64,14 @@ print( all.equal( estResultLAhom, estResultLAhomTX ) )
 estResultLAunr <- aidsEst( pNames, wNames, "xFood", hom = FALSE, sym = FALSE,
    data = Blanciforti86[ set, ], method = "LA:SL" )
 print( estResultLAunr )
+print( summary( estResultLAunr ) )
 print( elas( estResultLAunr, formula = "Ch", quantNames = wNames ) )
 # imposing restrictions via TX
 estResultLAunrTX <- aidsEst( pNames, wNames, "xFood", hom = FALSE, sym = FALSE,
    data = Blanciforti86[ set, ], method = "LA:SL",
    TX = TRUE )
 print( estResultLAunrTX )
+print( summary( estResultLAunrTX ) )
 print( elas( estResultLAunrTX, formula = "Ch", quantNames = wNames ) )
 estResultLAunrTX$call <- NULL
 estResultLAunrTX$est$bt <- NULL
@@ -85,11 +91,13 @@ cat( paste( "\nRepeating the evaluation of different elasticity formulas",
 estResultAIDS <- aidsEst( pNames, wNames, "xFood",
    data = Blanciforti86[ setWo1, ], method = "IL:L" )
 print( estResultAIDS )
+print( summary( estResultAIDS ) )
 print( elas( estResultAIDS, formula = "AIDS", quantNames = wNames ) )
 # imposing restrictions via TX
 estResultAIDSTX <- aidsEst( pNames, wNames, "xFood",
    data = Blanciforti86[ setWo1, ], method = "IL:L", TX = TRUE )
 print( estResultAIDSTX )
+print( summary( estResultAIDSTX ) )
 print( elas( estResultAIDSTX, formula = "AIDS", quantNames = wNames ) )
 estResultAIDSTX$call <- NULL
 estResultAIDSTX$est$bt <- NULL
@@ -107,11 +115,13 @@ print( all.equal( estResultAIDS, estResultAIDSTX ) )
 estResultAIDShom <- aidsEst( pNames, wNames, "xFood", sym = FALSE,
    data = Blanciforti86[ setWo1, ], method = "IL:L" )
 print( estResultAIDShom )
+print( summary( estResultAIDShom ) )
 print( elas( estResultAIDShom, formula = "AIDS", quantNames = wNames ) )
 # imposing restrictions via TX
 estResultAIDShomTX <- aidsEst( pNames, wNames, "xFood", sym = FALSE,
    data = Blanciforti86[ setWo1, ], method = "IL:L", TX = TRUE )
 print( estResultAIDShomTX )
+print( summary( estResultAIDShomTX ) )
 print( elas( estResultAIDShomTX, formula = "AIDS", quantNames = wNames ) )
 estResultAIDShomTX$call <- NULL
 estResultAIDShomTX$est$bt <- NULL
@@ -129,11 +139,13 @@ print( all.equal( estResultAIDShom, estResultAIDShomTX ) )
 estResultAIDSunr <- aidsEst( pNames, wNames, "xFood", hom = FALSE, sym = FALSE,
    data = Blanciforti86[ setWo1, ], method = "IL:L" )
 print( estResultAIDSunr )
+print( summary( estResultAIDSunr ) )
 print( elas( estResultAIDSunr, formula = "AIDS", quantNames = wNames ) )
 # imposing restrictions via TX
 estResultAIDSunrTX <- aidsEst( pNames, wNames, "xFood", hom = FALSE, sym = FALSE,
    data = Blanciforti86[ setWo1, ], method = "IL:L", TX = TRUE )
 print( estResultAIDSunrTX )
+print( summary( estResultAIDSunrTX ) )
 print( elas( estResultAIDSunrTX, formula = "AIDS", quantNames = wNames ) )
 estResultAIDSunrTX$call <- NULL
 estResultAIDSunrTX$est$bt <- NULL
@@ -152,23 +164,27 @@ estResultLaSNa <- aidsEst( pNames, wNames, "xFood",
    data = Blanciforti86,
    method = "LA:S" )
 print( estResultLaSNa )
+print( summary( estResultLaSNa ) )
 print( elas( estResultLaSNa, formula = "AIDS", quantNames = wNames ) )
 
 estResultLaSlNa <- aidsEst( pNames, wNames, "xFood",
    data = Blanciforti86,
    method = "LA:SL" )
 print( estResultLaSlNa )
+print( summary( estResultLaSlNa ) )
 print( elas( estResultLaSlNa, formula = "AIDS", quantNames = wNames ) )
 
 estResultLaLNa <- aidsEst( pNames, wNames, "xFood",
    data = Blanciforti86,
    method = "LA:L" )
 print( estResultLaLNa )
+print( summary( estResultLaLNa ) )
 print( elas( estResultLaLNa, formula = "AIDS", quantNames = wNames ) )
 
 estResultAIDSNa <- aidsEst( pNames, wNames, "xFood",
    data = Blanciforti86, method = "IL:L" )
 print( estResultAIDSNa )
+print( summary( estResultAIDSNa ) )
 print( elas( estResultAIDSNa, formula = "AIDS", quantNames = wNames ) )
 
 
