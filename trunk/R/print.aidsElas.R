2d13db0f718eb9727e2bc5cf45e4e2a7c464bfdf
suapm095 <unknown@noreply.com>
2007-05-01 19:50:29 +0000

    print.aidsElas prints now an explanation instead of the abbreviations "GA", "B1", "B2" and "Go"
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@449 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/R/print.aidsElas.R
+++ trunk/R/print.aidsElas.R
@@ -1,12 +1,16 @@
 print.aidsElas <- function( x, ... ) {
 
    cat( "\nDemand Elasticities " )
-   if( x$method == "Ch" ) {
-      cat( "(formula of Chalfant / Goddard)\n" )
+   if( x$method %in% c( "GA", "B1" ) ) {
+      cat( "(formulas of Green and Alston / Buse (first set))\n" )
+   } else if( x$method %in% c( "B2" ) ) {
+      cat( "(formulas of Buse (second set))\n" )
+   } else if( x$method %in% c( "Go", "Ch" ) ) {
+      cat( "(formulas of Goddard / Chalfant)\n" )
    } else if( x$method == "EU" ) {
       cat( "(formula of Eales and Unnevehr)\n" )
    } else if( x$method == "AIDS" ) {
-      cat( "(original AIDS formula)\n" )
+      cat( "(original AIDS formulas)\n" )
    } else {
       cat( "(unknown formula '", x$method, "')\n", sep = "" )
    }
