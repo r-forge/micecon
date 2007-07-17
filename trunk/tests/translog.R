713ee821f7d754ba21b93e3e8d039c052df6fe48
suapm095 <suapm095@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2007-07-17 21:32:19 +0000

    added tests for translogDeriv() and translogHessian() with argument "yName"
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@467 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/tests/translog.R
+++ trunk/tests/translog.R
@@ -25,19 +25,43 @@ all.equal( fitted, estResult$fitted )
 ## testing translogDeriv
 margProducts <- translogDeriv( c( "qLabor", "land", "qVarInput", "time" ),
    germanFarms, estResult$allCoef, estResult$allCoefCov )
-
 print( margProducts )
+
+margProductsObs <- translogDeriv( c( "qLabor", "land", "qVarInput", "time" ),
+   germanFarms, estResult$allCoef, estResult$allCoefCov, yName = "qOutput" )
+print( margProductsObs )
+
+germanFarms$fitted <- fitted
+margProductsFitted <- translogDeriv( c( "qLabor", "land", "qVarInput", "time" ),
+   germanFarms, estResult$allCoef, estResult$allCoefCov, yName = "fitted" )
+all.equal( margProducts, margProductsFitted )
+
 
 ## testing translogHessian
 # compute the Hessian matrices
 hessians <- translogHessian( c( "qLabor", "land", "qVarInput", "time" ),
    germanFarms, estResult$allCoef )
-
 print( hessians )
+
+hessiansObs <- translogHessian( c( "qLabor", "land", "qVarInput", "time" ),
+   germanFarms, estResult$allCoef, yName = "qOutput" )
+print( hessiansObs )
+
+germanFarms$fitted <- fitted
+hessiansFitted <- translogHessian( c( "qLabor", "land", "qVarInput", "time" ),
+   germanFarms, estResult$allCoef, yName = "fitted" )
+all.equal( hessians, hessiansFitted )
 
 # compute the bordered Hessian matrices
 borderedHessians <- translogHessian( c( "qLabor", "land", "qVarInput", "time" ),
    germanFarms, estResult$allCoef, bordered = TRUE )
-
 print( borderedHessians )
 
+borderedHessiansObs <- translogHessian( c( "qLabor", "land", "qVarInput", "time" ),
+   germanFarms, estResult$allCoef, yName = "qOutput", bordered = TRUE )
+print( borderedHessiansObs )
+
+germanFarms$fitted <- fitted
+borderedHessiansFitted <- translogHessian( c( "qLabor", "land", "qVarInput", "time" ),
+   germanFarms, estResult$allCoef, yName = "fitted", bordered = TRUE )
+all.equal( borderedHessians, borderedHessiansFitted )
