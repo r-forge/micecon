58d4c5e5b061f3c99e2675bde39ab0aed533e68f
suapm095 <suapm095@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2008-01-02 19:09:52 +0000

    removed the 0.1 branch of micEcon, because it not maintained any more
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@507 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- branches/0.1/R/checkNames.R
+++
@@ -1,9 +0,0 @@
-checkNames <- function( testNames, allNames ) {
-   inAllNames   <- testNames %in% allNames
-   if( !all( inAllNames ) ) {
-      stop( paste( "Object(s) '",
-         paste( testNames[ !inAllNames ], collapse = "', '" ),
-         "' not found.", sep = "" ) )
-   }
-}
-
