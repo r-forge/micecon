90a4452e4ade33ac402c4b243dc68fc44ac2f941
suapm095 <suapm095@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2008-02-28 14:20:39 +0000

    some corrections in aidsConsist()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@637 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/micEcon/R/aidsConsist.R
+++ trunk/micEcon/R/aidsConsist.R
@@ -5,17 +5,17 @@ aidsConsist <- function( priceNames, totExpName, coef, data,
    result <- list()
 
    result$addingUp <- all(
-      all.equal( sum( coef$alpha ), 1 ),
-      all.equal( sum( coef$beta ), 0 ),
+      all.equal( sum( coef$alpha ), 1 ) == TRUE,
+      all.equal( sum( coef$beta ), 0 ) == TRUE,
       all.equal( colSums( coef$gamma ), rep( 0, 4 ),
-         check.attributes = FALSE )
+         check.attributes = FALSE ) == TRUE
       )
 
    result$homogeneity <- all.equal( rowSums( coef$gamma ),
-      rep( 0, 4 ), check.attributes = FALSE )
+      rep( 0, 4 ), check.attributes = FALSE ) == TRUE
 
    result$symmetry <- isSymmetric( coef$gamma, tol = 1e-10,
-      check.attributes = FALSE )
+      check.attributes = FALSE ) == TRUE
 
    result$mono <- aidsMono( priceNames = priceNames, totExpName = totExpName,
       data = data, coef = coef, priceIndex = priceIndex,
