c90ed3882b66f7c682fbc40757331a921c3d288a
suapm095 <suapm095@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2008-02-28 13:59:02 +0000

    rewritten function aidsConsist() and print.aidsConsist(): aidsConsist() additionally checks the adding-up, homogeneity and symmetry condition now; aidsConsist() stores the entire objects returned by aidsMono() and aidsConcav() in separate lists now; print.aidsMono() and print.aidsConcav() are used to print the results of the monotonicity and concavity checks now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@634 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/micEcon/R/aidsConsist.R
+++ trunk/micEcon/R/aidsConsist.R
@@ -1,20 +1,31 @@
-aidsConsist <- function( priceNames, totExpName, data, coef,
+aidsConsist <- function( priceNames, totExpName, coef, data,
       priceIndex = "TL", basePrices = NULL, baseShares = NULL,
       shareNames = NULL ) {
 
-   monoResult <- aidsMono( priceNames = priceNames, totExpName = totExpName,
+   result <- list()
+
+   result$addingUp <- all(
+      all.equal( sum( coef$alpha ), 1 ),
+      all.equal( sum( coef$beta ), 0 ),
+      all.equal( colSums( coef$gamma ), rep( 0, 4 ),
+         check.attributes = FALSE )
+      )
+
+   result$homogeneity <- all.equal( rowSums( coef$gamma ),
+      rep( 0, 4 ), check.attributes = FALSE )
+
+   result$symmetry <- isSymmetric( coef$gamma, tol = 1e-10,
+      check.attributes = FALSE )
+
+   result$mono <- aidsMono( priceNames = priceNames, totExpName = totExpName,
       data = data, coef = coef, priceIndex = priceIndex,
       basePrices = basePrices, baseShares = baseShares )
 
-   concResult <- aidsConcav( priceNames = priceNames, totExpName = totExpName,
-      data = data, coef = coef, shareNames = shareNames )
-
-   result <- list()
-   result$mPercent <- monoResult$monoPercent
-   result$monotony <- monoResult$monotony
-   result$cPercent <- concResult$concavPercent
-   result$concavity <- concResult$concavity
-   result$cMatrices <- concResult$cMatrices
+   if( priceIndex == "TL" ) {
+      result$concav <- aidsConcav( priceNames = priceNames,
+         totExpName = totExpName, data = data, coef = coef,
+         shareNames = shareNames )
+   }
 
    class( result ) <- "aidsConsist"
    return( result )
