6e962ae9214bd8bf64c930c2584fba67b709b91b
suapm095 <unknown@noreply.com>
2008-02-28 13:53:21 +0000

    print.aidsMono() and print.aidsConcav() have an additional argument "header" now: it is TRUE by default; if it is FALSE, the header line is not printed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@633 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/micEcon/R/print.aidsConcav.R
+++ trunk/micEcon/R/print.aidsConcav.R
@@ -1,6 +1,8 @@
-print.aidsConcav <- function( x, ... ) {
-   cat( "\nChecking the concavity condition of an " )
-   cat( "Almost Ideal Demand System (AIDS):\n" )
+print.aidsConcav <- function( x, header = TRUE, ... ) {
+   if( header ) {
+      cat( "\nChecking the concavity condition of an " )
+      cat( "Almost Ideal Demand System (AIDS):\n" )
+   }
    cat( "Concavity is fulfilled at " )
    cat( x$nConcavObs, "out of", x$nValidObs, "observations" )
    cat( " (", x$concavPercent, "%)\n", sep = "" )
