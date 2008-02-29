64355a45695b6c2d098fd93bd60f6dba4883ab89
suapm095 <unknown@noreply.com>
2008-02-29 13:20:58 +0000

    improved checkConsist.aidsEst(); added optional argument observedShares (FALSE by default) to checkConsist.aidsEst()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@640 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/micEcon/R/checkConsist.R
+++ trunk/micEcon/R/checkConsist.R
@@ -2,13 +2,35 @@ checkConsist <- function( object, ... ) {
     UseMethod( "checkConsist" )
 }
 
-checkConsist.aidsEst <- function( object, ... ) {
+checkConsist.aidsEst <- function( object, observedShares = FALSE, ... ) {
+
+   # price index for checking monotonicity
+   if( object$method == "LA" ) {
+      if( observedShares ) {
+         priceIndex <- object$lnp
+      } else {
+         priceIndex <- object$priceIndex
+      }
+   } else if( object$method %in% c( "IL", "MK" ) ) {
+      priceIndex = "TL"
+   } else {
+      stop( "unknown element 'method' of argument 'object'" )
+   }
+
+   # shares for checking concavity
+   if( observedShares ) {
+      shareNames <- object$shareNames
+   } else {
+      shareNames <- NULL
+   }
+
+
    aidsConsist( priceNames = object$priceNames,
-      shareNames = object$shareNames,
+      shareNames = shareNames,
       totExpName = object$totExpName,
-      data = get( as.character( object$call$data ) ),
+      data = eval( object$call$data ),
       coef = object$coef,
-      priceIndex = object$priceIndex,
+      priceIndex = priceIndex,
       basePrices = object$basePrices,
       baseShares = object$baseShares )
 }
