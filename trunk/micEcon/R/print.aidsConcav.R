d1ccce7856b90bb885497470017fbbd60b72d317
suapm095 <unknown@noreply.com>
2008-03-01 08:57:17 +0000

    added missing "invisible( x )" in two print methods
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@656 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/micEcon/R/print.aidsConcav.R
+++ trunk/micEcon/R/print.aidsConcav.R
@@ -6,4 +6,6 @@ print.aidsConcav <- function( x, header = TRUE, ... ) {
    cat( "Concavity is fulfilled at " )
    cat( x$nConcavObs, "out of", x$nValidObs, "observations" )
    cat( " (", x$concavPercent, "%)\n", sep = "" )
+
+   invisible( x )
 }
