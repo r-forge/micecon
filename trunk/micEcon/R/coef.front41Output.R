510bf2a34ba2e37d843fdb2843435d0ef47c358b
suapm095 <unknown@noreply.com>
2008-06-02 13:19:47 +0000

    added "summary" and "coef.summary" methods for objectr returned by "front41ReadOutput"; added tests for these methods
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@731 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/micEcon/R/coef.front41Output.R
+++ trunk/micEcon/R/coef.front41Output.R
@@ -13,6 +13,21 @@ coef.front41Output <- function( object, which = "MLE", ... ) {
    return( result )
 }
 
+coef.summary.front41Output <- function( object, which = "MLE", ... ) {
+
+   if( which %in% c( "OLS", "ols" ) ) {
+      result <- object$olsResults
+   } else if( which %in% c( "GRID", "Grid", "grid" ) ) {
+      result <- object$gridResults
+   } else if( which %in% c( "MLE", "mle" ) ) {
+      result <- object$mleResults
+   } else {
+      stop( "argument 'which' must be either 'OLS', 'GRID', or 'MLE'" )
+   }
+
+   return( result )
+}
+
 vcov.front41Output <- function( object, ... ) {
    return( object$mleCov )
 }
