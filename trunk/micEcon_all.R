60344fd3a4d5e2c0a91a1acf308df105fee5c0f1
suapm095 <suapm095@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2008-09-23 13:36:06 +0000

    removed /trunk (including micEcon_all.R)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@993 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- trunk/micEcon_all.R
+++
@@ -1,9 +0,0 @@
-path <- "micEcon/R/"
-micEconFiles <- list.files( path, all.files = TRUE )
-micEconFiles <- grep( ".*\\.R$", micEconFiles, value = TRUE )
-for( i in seq( along = micEconFiles ) ) {
-   returnedByTry <- try( source( paste( path, micEconFiles[ i ], sep = "" ) ) )
-   if( class( returnedByTry ) == "try-error" ) {
-      cat( paste( micEconFiles[ i ], ": ", returnedByTry, sep = "" ) )
-   }
-}
