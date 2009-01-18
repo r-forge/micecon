90b67c1111df4bb83d21089cbf591c305ea31811
arne <arne@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2009-01-18 00:05:15 +0000

    corrected bug in elas.translogEst(): the argument "dataLogged" used to create "object" is extracted and passed to translogEla() now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@1088 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- pkg/R/elas.translogEst.R
+++ pkg/R/elas.translogEst.R
@@ -1,12 +1,20 @@
-elas.translogEst <- function( object, data = NULL, ... ) {
+elas.translogEst <- function( object, data = NULL, dataLogged = NULL, ... ) {
 
    if( is.null( data ) ) {
       data <- eval( object$call$data )
    }
 
+   if( is.null( dataLogged ) ) {
+      if( is.null( object$call$dataLogged ) ) {
+         dataLogged <- FALSE
+      } else {
+         dataLogged <- object$call$dataLogged
+      }
+   }
+
    result <- translogEla( xNames = eval( object$call$xNames ), 
       data = data, coef = coef( object ), 
-      coefCov = vcov( object ) )
+      coefCov = vcov( object ), dataLogged = dataLogged )
 
    return( result )
 }
