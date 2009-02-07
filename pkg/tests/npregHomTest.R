b53b04f17629d89d54f8705c203f422222bc8500
arne <arne@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2009-02-07 01:27:02 +0000

    added tests for npregHom() with argument "restrictGrad" equal to "FALSE"
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@1139 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- pkg/tests/npregHomTest.R
+++ pkg/tests/npregHomTest.R
@@ -17,7 +17,7 @@ weights <- c(
    pLabor = mean( germanFarms$qLabor ) )
 weights <- weights / sum( weights )
 
-# estimation
+# estimation (restricted gradients)
 npseed( 123 )
 estResult <- npregHom( "qVarInput",
    xNames = c( "pOutput", "pVarInput", "pLabor", "land" ),
@@ -36,7 +36,25 @@ all.equal( estResult2$grad[ , "pOutput" ] * germanFarms$pOutput +
    estResult2$grad[ , "pVarInput" ] * germanFarms$pVarInput,
    - estResult2$grad[ , "pLabor" ] * germanFarms$pLabor )
 
-# estimation with Epanechnikov kernel
+# estimation (gradients not restricted)
+npseed( 123 )
+estResultAll <- npregHom( "qVarInput",
+   xNames = c( "pOutput", "pVarInput", "pLabor", "land" ),
+   data = germanFarms, homWeights = weights, restrictGrad = FALSE )
+print( estResultAll )
+all.equal( estResultAll$grad[ , "pOutput" ] * germanFarms$pOutput +
+   estResultAll$grad[ , "pVarInput" ] * germanFarms$pVarInput,
+   - estResultAll$grad[ , "pLabor" ] * germanFarms$pLabor )
+# different order of weights
+npseed( 123 )
+estResultAll2 <- npregHom( "qVarInput",
+   xNames = c( "pOutput", "pVarInput", "pLabor", "land" ),
+   data = germanFarms, homWeights = weights[ c( 3, 2, 1 ) ],
+   restrictGrad = FALSE )
+all.equal( estResultAll$grad, estResultAll2$grad, tolerance = 1e-6 )
+
+
+# estimation with Epanechnikov kernel (restricted gradients)
 npseed( 123 )
 estResultEpa <- npregHom( "qVarInput",
    xNames = c( "pOutput", "pVarInput", "pLabor", "land" ),
@@ -46,7 +64,26 @@ all.equal( estResultEpa$grad[ , "pOutput" ] * germanFarms$pOutput +
    estResultEpa$grad[ , "pVarInput" ] * germanFarms$pVarInput,
    - estResultEpa$grad[ , "pLabor" ] * germanFarms$pLabor )
 
-# estimation with manual bandwidth selection
+# estimation with Epanechnikov kernel (gradients not restricted)
+npseed( 123 )
+estResultEpaAll <- npregHom( "qVarInput",
+   xNames = c( "pOutput", "pVarInput", "pLabor", "land" ),
+   data = germanFarms, homWeights = weights, restrictGrad = FALSE,
+   ckertype="epanechnikov" )
+print( estResultEpaAll )
+all.equal( estResultEpaAll$grad[ , "pOutput" ] * germanFarms$pOutput +
+   estResultEpaAll$grad[ , "pVarInput" ] * germanFarms$pVarInput,
+   - estResultEpaAll$grad[ , "pLabor" ] * germanFarms$pLabor )
+# different order of weights
+npseed( 123 )
+estResultEpaAll2 <- npregHom( "qVarInput",
+   xNames = c( "pOutput", "pVarInput", "pLabor", "land" ),
+   data = germanFarms, homWeights = weights[ c( 3, 2, 1 ) ],
+   restrictGrad = FALSE, ckertype="epanechnikov" )
+all.equal( estResultEpaAll$grad, estResultEpaAll2$grad, tolerance = 1e-6 )
+
+
+# estimation with manual bandwidth selection (restricted gradients)
 estResultMan <- npregHom( "qVarInput",
    xNames = c( "pOutput", "pVarInput", "pLabor", "land" ),
    data = germanFarms, homWeights = weights, bws = rep( 1, 3 ),
@@ -64,3 +101,19 @@ print( estResultMan2 )
 all.equal( estResultMan2$grad[ , "pOutput" ] * germanFarms$pOutput +
    estResultMan2$grad[ , "pVarInput" ] * germanFarms$pVarInput,
    - estResultMan2$grad[ , "pLabor" ] * germanFarms$pLabor )
+
+# estimation with manual bandwidth selection (gradients not restricted)
+estResultManAll <- npregHom( "qVarInput",
+   xNames = c( "pOutput", "pVarInput", "pLabor", "land" ),
+   data = germanFarms, homWeights = weights, restrictGrad = FALSE,
+   bws = rep( 1, 4 ), bwscaling = TRUE )
+print( estResultManAll )
+all.equal( estResultManAll$grad[ , "pOutput" ] * germanFarms$pOutput +
+   estResultManAll$grad[ , "pVarInput" ] * germanFarms$pVarInput,
+   - estResultManAll$grad[ , "pLabor" ] * germanFarms$pLabor )
+# different order of weights
+estResultManAll2 <- npregHom( "qVarInput",
+   xNames = c( "pOutput", "pVarInput", "pLabor", "land" ),
+   data = germanFarms, homWeights = weights[ c( 3, 2, 1 ) ],
+   restrictGrad = FALSE, bws = rep( 1, 4 ), bwscaling = TRUE )
+all.equal( estResultManAll$grad, estResultManAll2$grad )
