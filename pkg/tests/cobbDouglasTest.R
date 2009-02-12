4b2fdeeb14eefa134e6e4c1f397fe8d66430de9a
arne <arne@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2009-02-12 18:45:37 +0000

    added tests for cobbDouglasCalc()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@1150 95fa4d17-b1e8-0310-9d42-a43d67248e42

---
+++ pkg/tests/cobbDouglasTest.R
@@ -0,0 +1,28 @@
+library( micEcon )
+
+data( germanFarms )
+# output quantity:
+germanFarms$qOutput <- germanFarms$vOutput / germanFarms$pOutput
+# quantity of variable inputs
+germanFarms$qVarInput <- germanFarms$vVarInput / germanFarms$pVarInput
+# a time trend to account for technical progress:
+germanFarms$time <- c(1:20)
+
+# estimate a Cobb-Douglas production function
+estResult <- translogEst( "qOutput", c( "qLabor", "land", "qVarInput", "time" ),
+   germanFarms, linear = TRUE )
+
+# calculate fitted values
+fitted <- cobbDouglasCalc( c( "qLabor", "land", "qVarInput", "time" ),
+   data = germanFarms, coef = coef( estResult )[ 1:5 ] )
+print( fitted )
+all.equal( fitted, estResult$fitted )
+
+# calculate fitted values using logged independent variables
+germanFarms$lQLabor    <- log( germanFarms$qLabor )
+germanFarms$lLand      <- log( germanFarms$land )
+germanFarms$lQVarInput <- log( germanFarms$qVarInput )
+germanFarms$lTime      <- log( germanFarms$time )
+fittedLogged <- cobbDouglasCalc( c( "lQLabor", "lLand", "lQVarInput", "lTime" ),
+   data = germanFarms, coef = coef( estResult )[ 1:5 ], dataLogged = TRUE )
+all.equal( fitted, exp( fittedLogged ) )
