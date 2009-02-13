fcb830f8aff681b0cc578793a516a424075e2b38
arne <arne@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2009-02-13 11:20:14 +0000

    changed order of inputs (this will make it easier to add tests for cobbDouglasOpt())
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@1154 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- pkg/tests/cobbDouglasTest.R
+++ pkg/tests/cobbDouglasTest.R
@@ -9,11 +9,11 @@ germanFarms$qVarInput <- germanFarms$vVarInput / germanFarms$pVarInput
 germanFarms$time <- c(1:20)
 
 # estimate a Cobb-Douglas production function
-estResult <- translogEst( "qOutput", c( "qLabor", "land", "qVarInput", "time" ),
+estResult <- translogEst( "qOutput", c( "qLabor", "qVarInput", "land", "time" ),
    germanFarms, linear = TRUE )
 
 # calculate fitted values
-fitted <- cobbDouglasCalc( c( "qLabor", "land", "qVarInput", "time" ),
+fitted <- cobbDouglasCalc( c( "qLabor", "qVarInput", "land", "time" ),
    data = germanFarms, coef = coef( estResult )[ 1:5 ] )
 print( fitted )
 all.equal( fitted, estResult$fitted )
@@ -23,19 +23,19 @@ germanFarms$lQLabor    <- log( germanFarms$qLabor )
 germanFarms$lLand      <- log( germanFarms$land )
 germanFarms$lQVarInput <- log( germanFarms$qVarInput )
 germanFarms$lTime      <- log( germanFarms$time )
-fittedLogged <- cobbDouglasCalc( c( "lQLabor", "lLand", "lQVarInput", "lTime" ),
+fittedLogged <- cobbDouglasCalc( c( "lQLabor", "lQVarInput", "lLand", "lTime" ),
    data = germanFarms, coef = coef( estResult )[ 1:5 ], dataLogged = TRUE )
 all.equal( fitted, exp( fittedLogged ) )
 
 # coefficients not named
 coefNoNames <- coef( estResult )[ 1:5 ]
 names( coefNoNames ) <- NULL
-fittedNoNames <- cobbDouglasCalc( c( "qLabor", "land", "qVarInput", "time" ),
+fittedNoNames <- cobbDouglasCalc( c( "qLabor", "qVarInput", "land", "time" ),
    data = germanFarms, coef = coefNoNames )
 all.equal( fitted, fittedNoNames )
 
 # coefficients in a different order
 coefDiffOrder <- coef( estResult )[ c( 3, 5, 1, 2, 4 ) ]
-fittedDiffOrder <- cobbDouglasCalc( c( "qLabor", "land", "qVarInput", "time" ),
+fittedDiffOrder <- cobbDouglasCalc( c( "qLabor", "qVarInput", "land", "time" ),
    data = germanFarms, coef = coefDiffOrder )
 all.equal( fitted, fittedDiffOrder )
