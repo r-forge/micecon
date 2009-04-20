627ca5d11fa94a9a01578adde89adf1dac16e2fd
arne (Arne Henningsen) <arne.henningsen@gmail.com>
2009-04-20 16:02:08 +0000

    translogProdFuncMargCost() can calculate the marginal costs of the outputs of a translog ray production function with two outputs now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@1200 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- branches/geraldine/R/translogProdFuncMargCost.R
+++ branches/geraldine/R/translogProdFuncMargCost.R
@@ -1,10 +1,13 @@
-translogProdFuncMargCost <- function( yName, xNames, wNames,
+translogProdFuncMargCost <- function( yNames, xNames, wNames,
       data, coef, dataLogged = FALSE ) {
 
-   checkNames( c( yName, xNames, wNames ), names( data ) )
+   checkNames( c( yNames, xNames, wNames ), names( data ) )
 
-   if( length( yName ) != 1 ) {
-      stop( "argument 'yName' must include only one input" )
+   if( length( yNames ) > 2 ) {
+      stop( "the estimation of ray functions has not been implemented",
+         " with more than two outputs yet" )
+   } else if( ! length( yNames ) %in% c( 1, 2 ) ) {
+      stop( "argument 'yNames' must include either one or two outputs" )
    }
 
    if( length( xNames ) != length( wNames ) ) {
@@ -15,14 +18,34 @@ translogProdFuncMargCost <- function( yName, xNames, wNames,
       logData   <- data
    } else {
       logData <- .micEconLogData( data = data,
-         varNames = c( yName, xNames, wNames ) )
+         varNames = c( yNames, xNames, wNames ) )
    }
 
    # number of inputs
    nInput <- length( xNames )
 
+   # coefficients with names as if theta was a normal input
+   coefTl <- coef
+   if( length( yNames ) > 1 ) {
+      coefNamesTl <- names( coef )
+      coefNamesTl[ coefNamesTl == "b_t_t" ] <-
+         paste( "b", nInput + 1, nInput + 1, sep = "_" )
+      coefNamesTl <- sub( "_t$", paste( "_", nInput + 1, sep = "" ),
+         coefNamesTl )
+      names( coefTl ) <- coefNamesTl
+   }
+
+   # calculate distance and theta
+   xNamesTheta <- xNames
+   if( length( yNames ) >  1 ) {
+      distance <- sqrt( data[[ yNames[ 1 ] ]]^2 + data[[ yNames[ 2 ] ]]^2 )
+      logData$distance <- log( distance )
+      logData$theta <- acos( data[[ yNames[ 1 ] ]] / distance )
+      xNamesTheta <- c( xNamesTheta, "theta" )
+   }
+
    # elasticities: d log F / d log x_i
-   elaData <- translogEla( xNames = xNames, data = logData, coef = coef,
+   elaData <- translogEla( xNames = xNamesTheta, data = logData, coef = coefTl,
       dataLogged = TRUE )
 
    # matrix of beta coefficients
@@ -35,13 +58,16 @@ translogProdFuncMargCost <- function( yName, xNames, wNames,
    }
 
    # marginal costs
-   margCost <- matrix( NA, nrow = nrow( logData ), ncol = 1 )
+   margCost <- matrix( NA, nrow = nrow( logData ), ncol = length( yNames ) )
 
    for( t in 1:nrow( logData ) ) {
-      yVal <- exp( as.numeric( logData[ t, yName ] ) )
+      yVal <- exp( as.numeric( logData[ t, yNames ] ) )
       xVal <- exp( as.numeric( logData[ t, xNames ] ) )
       wVal <- exp( as.numeric( logData[ t, wNames ] ) )
       eVal <- as.numeric( elaData[ t, ] )
+      if( length( yNames ) > 1 ) {
+         dVal <- distance[ t ]
+      }
 
       # Jacobian matrix of g with respect to x
       gxJac <- matrix( NA, nrow = nInput, ncol = nInput )
@@ -63,9 +89,37 @@ translogProdFuncMargCost <- function( yName, xNames, wNames,
       }
 
       # Jacobian matrix of g with respect to y
-      gyJac <- matrix( nrow = nInput, ncol = 1 )
-      gyJac[ 1:( nInput - 1 ), 1 ] <- 0
-      gyJac[ nInput, 1 ] <- - 1 / yVal
+      gyJac <- matrix( nrow = nInput, ncol = length( yNames ) )
+      if( length( yNames ) == 1 ) {
+         gyJac[ 1:( nInput - 1 ), 1 ] <- 0
+         gyJac[ nInput, 1 ] <- - 1 / yVal
+      } else {
+         # derivatives of theta with respect to the outputs
+         derivThetaY <- rep( NA, length( yNames ) )
+         for( j in 1:length( yNames ) ) {
+            derivThetaY[ j ] <-
+               ( yVal[ 1 ] * yVal[ j ] / dVal^3 - ( j == 1 ) / dVal ) /
+               ( sqrt( 1 - ( yVal[ 1 ] / dVal )^2 ) )
+         }
+         # derivatives of theta with respect to the outputs
+         derivDistanceY <- rep( NA, length( yNames ) )
+         for( j in 1:length( yNames ) ) {
+            derivDistanceY[ j ] <- yVal[ j ] / dVal^2
+         }
+         # and now the Jacobian ...
+         for( j in 1:length( yNames ) ) {
+            for( i in 1:( nInput - 1 ) ) {
+               gyJac[ i, j ] <-
+                  ( derivThetaY[ j ] * wVal[ nInput ] * xVal[ nInput ] /
+                     ( xVal[ i ] * eVal[ nInput ] ) ) *
+                  ( ( eVal[ i ] / eVal[ nInput ] ) *
+                     coef[ paste( "b", nInput, "t", sep = "_" ) ] - 
+                     coef[ paste( "b", i, "t", sep = "_" ) ] )
+            }
+            gyJac[ nInput, j ] <- eVal[ nInput + 1 ] * derivThetaY[ j ] -
+               - derivDistanceY[ j ]
+         }
+      }
 
       # Jacobian matrix of x with respect to y
       xyJac <- - solve( gxJac, gyJac )
@@ -76,7 +130,7 @@ translogProdFuncMargCost <- function( yName, xNames, wNames,
 
    margCost <- as.data.frame( margCost )
    rownames( margCost ) <- rownames( data )
-   names( margCost ) <- yName
+   names( margCost ) <- yNames
 
    return( margCost )
 }
