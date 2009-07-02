4c546eaf968138ba199af91e63e573a07170de00
arne (Arne Henningsen) <arne.henningsen@gmail.com>
2009-07-02 19:53:06 +0000

    removed obsolete branch /branches/geraldine/ because all modifications have been merged into /pkg/ (mainly in revision 1215); /branches/geraldine/ and /pkg/ are identical except for the files DESCRIPTION and INDEX now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@1216 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- branches/geraldine/R/print.summary.translogEst.R
+++
@@ -1,9 +0,0 @@
-print.summary.translogEst <- function( x, ... ) {
-
-   cat( "Estimated Translog function with", x$nObs, "observations.\n" )
-   printCoefmat( x$coefTable )
-
-   cat( "R-squared:", x$r2, "    Adjusted R-squared:", x$r2bar, "\n" )
-
-   invisible( x )
-}
--- branches/geraldine/R/summary.translogEst.R
+++
@@ -1,8 +0,0 @@
-summary.translogEst <- function( object, ... ) {
-
-   object$coefTable <- coefTable( coef( object ),
-         diag( vcov( object ) )^0.5, df.residual( object$est ) )
-
-   class( object ) <- "summary.translogEst"
-   return( object )
-}
