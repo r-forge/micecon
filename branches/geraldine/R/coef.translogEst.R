e4921b1becc869f60392fb0b1e1863fe3aa75b92
arne <arne@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2009-07-02 19:53:06 +0000

    removed obsolete branch /branches/geraldine/ because all modifications have been merged into /pkg/ (mainly in revision 1215); /branches/geraldine/ and /pkg/ are identical except for the files DESCRIPTION and INDEX now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@1216 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- branches/geraldine/R/coef.translogEst.R
+++
@@ -1,7 +0,0 @@
-coef.translogEst <- function( object, ... ) {
-   return( coef.quadFuncEst( object ) )
-}
-
-vcov.translogEst <- function( object, ... ) {
-   return( vcov.quadFuncEst( object ) )
-}
