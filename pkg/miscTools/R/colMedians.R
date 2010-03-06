04bf1290047071a5c0a88b0847b7c93f5988d4e5
arne <arne@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2010-03-06 14:24:44 +0000

    function colMedians() can return the medians of each non-row of an *array* now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@1403 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- pkg/miscTools/R/colMedians.R
+++ pkg/miscTools/R/colMedians.R
@@ -3,23 +3,32 @@ colMedians <- function( x, na.rm = FALSE ) {
    if( is.data.frame( x ) ) {
       x <- as.matrix( x )
    }
-   if( !is.matrix( x ) ) {
-      stop( "argument 'x' must be a data.frame or a matrix" )
+   if( !is.array( x ) ) {
+      stop( "argument 'x' must be a data.frame, matrix, or array" )
    }
    if( !is.numeric( x ) ) {
       stop( "argument 'x' must be numeric" )
    }
 
-   result <- rep( NA, ncol( x ) )
-   names( result ) <- colnames( x )
+   result <- array( NA, dim = dim( x )[-1] )
+   dimnames( result ) <- dimnames( x )[-1]
 
-   for( i in 1:ncol( x ) ) {
-      result[ i ] <- median( x[ , i ], na.rm = na.rm )
+   for( i in 1:dim( x )[ 2 ] ) {
+      if( length( dim( x ) ) == 2 ) {
+         result[ i ] <- median( x[ , i ], na.rm = na.rm )
+      } else {
+         result[ slice.index( result, 1 ) == i ] <-
+            colMedians( array( x[ slice.index( x, 2 ) == i ],
+               dim = dim( x )[ -2 ] ), na.rm = na.rm )
+      }
    }
 
    return( result )
 }
 
 rowMedians <- function( x, na.rm = FALSE ) {
+   if( is.null( dim( x ) ) || length( dim( x ) ) != 2 ) {
+      stop( "argument 'x' must be a matrix or a data.frame" )
+   }
    return( colMedians( t( x ), na.rm = na.rm ) )
 }
