8c5c2467d9f2d5156cc57eb0c8b1d15271d6882a
arne <arne@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2010-03-06 14:26:04 +0000

    added tests for applying colMedians() to 3-dimensional and 4-dimansional arrays
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@1404 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- pkg/miscTools/tests/colMediansTest.R
+++ pkg/miscTools/tests/colMediansTest.R
@@ -26,3 +26,16 @@ print( rm2 )
 
 all.equal( cm2, rowMedians( t( Electricity ) ) )
 all.equal( rm2, colMedians( t( Electricity ) ) )
+
+# array (3 dimensions)
+a3 <- array( 1:24, dim = c(4,3,2),
+   dimnames = list( c("a","b","c","d"), c("A","B","C"), c("x","y") ) )
+colMedians( a3 )
+all.equal( median( a3[ , "B", "y" ] ), colMedians( a3 )[ "B", "y" ] )
+
+# array (4 dimensions)
+a4 <- array( 1:120, dim = c(5,4,3,2),
+   dimnames = list( c("a","b","c","d","e"), c("A","B","C","D"),
+   c("x","y","z"), c("Y","Z") ) )
+colMedians( a4 )
+all.equal( median( a4[ , "B", "x", "Z" ] ), colMedians( a4 )[ "B", "x", "Z" ] )
