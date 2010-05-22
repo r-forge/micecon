ba3f98ba05f4c0341990929e6b69bd5be150d447
arne (Arne Henningsen) <arne.henningsen@gmail.com>
2010-05-22 11:54:17 +0000

    translogCheckMono() now can check whether a translog function is monotonically decreasing in some explanatory variables and monotonically increasing in other explanatory variables; added tests for this new feature
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@1458 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- pkg/micEcon/R/translogCheckMono.R
+++ pkg/micEcon/R/translogCheckMono.R
@@ -2,6 +2,16 @@ translogCheckMono <- function( xNames, data, coef, increasing = TRUE,
    strict = FALSE, dataLogged = FALSE,
    tol = 10 * .Machine$double.eps ) {
 
+   if( !is.logical( increasing ) ) {
+      stop( "argument 'increasing' must be logical" )
+   } else if( length( increasing ) == 1 ) {
+      increasing <- rep( increasing, length( xNames ) )
+   } else if( length( increasing ) != length( xNames ) ) {
+      stop( "argument 'increasing' must be either a single logical variable",
+         " or a vector of logical variables with same length as argument",
+         " 'xNames' (", length( xNames ), ")" )
+   }
+
    result <- list()
 
    deriv <- translogDeriv( xNames = xNames, data = data, coef = coef,
@@ -15,9 +25,9 @@ translogCheckMono <- function( xNames, data, coef, increasing = TRUE,
 
    for( i in 1:nExog ) {
       if( strict ) {
-         result$exog[ , i ] <- deriv[ , i ] * (-1)^increasing < 0
+         result$exog[ , i ] <- deriv[ , i ] * (-1)^increasing[ i ] < 0
       } else {
-         result$exog[ , i ] <- deriv[ , i ] * (-1)^increasing <= tol
+         result$exog[ , i ] <- deriv[ , i ] * (-1)^increasing[ i ] <= tol
       }
    }
 
