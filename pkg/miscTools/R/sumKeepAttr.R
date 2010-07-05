d1df17b1dccc3143a61e600c8aab8654fc35246c
arne (Arne Henningsen) <arne.henningsen@gmail.com>
2010-07-05 16:24:45 +0000

    added argument "keepNames" to sumKeepAttr()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@1478 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- pkg/miscTools/R/sumKeepAttr.R
+++ pkg/miscTools/R/sumKeepAttr.R
@@ -1,6 +1,9 @@
 ## return the sum of an array while keeping its attributes
-sumKeepAttr <- function( x, na.rm = FALSE ) {
+sumKeepAttr <- function( x, keepNames = FALSE, na.rm = FALSE ) {
    xAttr <- attributes( x )
+   if( !keepNames ) {
+      xAttr$names <- NULL
+   }
    x <- sum( x, na.rm = na.rm )
    mostattributes( x ) <- xAttr
    return( x )
