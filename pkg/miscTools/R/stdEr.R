dc56755b09bbe51f567ac0099818a63ea5614682
arne <arne@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2010-09-02 10:40:58 +0000

    added generic function stdEr() including a default method and a method for objects of class "lm" (moved from the "maxLik" package to this package)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@1500 95fa4d17-b1e8-0310-9d42-a43d67248e42

---
+++ pkg/miscTools/R/stdEr.R
@@ -0,0 +1,21 @@
+### methods for extracting standard errors from the models
+
+stdEr <- function(x, ...)
+    ## Extract standard deviations from models (as coefficients)
+    UseMethod("stdEr")
+
+stdEr.default <- function(x, ...) {
+   if(!is.null(x$std))
+       return(x$std)
+   if(!is.null(vc <- vcov(x))) {
+      s <- sqrt(diag(vc))
+      names(s) <- names(coef(x))
+      return(s)
+   }
+   return(NULL)
+                           # if neither std nor vcov is defined, we return NULL...
+}
+
+stdEr.lm <- function(x, ...)
+    sqrt(diag(vcov(x)))
+
