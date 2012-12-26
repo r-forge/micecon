5a7a1d67387faaee610f1196ef8da55b56fe7161
arne <arne@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2012-12-26 14:28:16 +0000

    changed test case for compPlot() so that the plot in logarithmic scales does not have a 45 degree line
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@1859 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- pkg/miscTools/tests/compPlotTest.R
+++ pkg/miscTools/tests/compPlotTest.R
@@ -2,8 +2,8 @@ library( "miscTools" )
 library( "graphicsQC" )
 
 set.seed( 123 )
-x <- runif( 25 )
-y <- 10 + 3 * x + rnorm( 25 )
+x <- 1000 * runif( 25 )
+y <- 10 + 3 * x + 100 * rnorm( 25 )
 ols <- lm( y ~ x )
 
 obsLab <- "observed"
@@ -11,13 +11,13 @@ fitLab <- "fitted"
 xyString <- "xy"
 
 expressions <- c( "compPlot( y, fitted( ols ) )",
-   "compPlot( y, fitted( ols ), lim = c( 0, 25 ) )",
+   "compPlot( y, fitted( ols ), lim = c( 0, 3500 ) )",
    "compPlot( y, fitted( ols ), pch = 20 )",
    "compPlot( y, fitted( ols ), xlab = obsLab, ylab = fitLab )",
    "compPlot( y, fitted( ols ), log = xyString )" )
 
 # generate control plots:                  
-# plotExpr( expressions, path = "tests/controlPlots", prefix = "compPlot" )
+# plotExpr( expressions, path = "tests/controlPlots", prefix = "compPlot", clear = TRUE )
 
 plotExpr( expressions, path = "testPlots", prefix = "compPlot" )
 
