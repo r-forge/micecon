098a3e229e38d0aa6fbf8edf8a2f48927e9af26f
arne (Arne Henningsen) <arne.henningsen@gmail.com>
2013-09-30 14:48:05 +0000

    added tests of function npregGradFactor()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@1891 95fa4d17-b1e8-0310-9d42-a43d67248e42

---
+++ pkg/micEconNP/tests/npregGradFactorTest.R
@@ -0,0 +1,18 @@
+library( "micEconNP" )
+data( "oecdpanel" )
+
+oecdpanel$yearFactor <- factor( oecdpanel$year )
+
+npModel <- npreg( growth ~ yearFactor + initgdp, 
+   regtype = "ll", gradients = TRUE, data = oecdpanel )
+
+round( gradients( npModel ), 3 )
+
+yearGrad <- npregGradFactor( npModel, "yearFactor", oecdpanel )
+
+round( yearGrad, 3 )
+
+round( colMeans( yearGrad, na.rm = TRUE ), 4 )
+
+round( colMedians( yearGrad, na.rm = TRUE ), 4 )
+
