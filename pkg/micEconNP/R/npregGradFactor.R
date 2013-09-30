d8e64ccee6f3ebdc4c34d1f1233dd2005f5d6783
arne <arne@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2013-09-30 22:39:47 +0000

    npregGradFactor() no longer relies on the internally stored gradients (that are only returned if the model was estimated with argument "gradients" equal to TRUE)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@1897 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- pkg/micEconNP/R/npregGradFactor.R
+++ pkg/micEconNP/R/npregGradFactor.R
@@ -2,10 +2,7 @@ npregGradFactor <- function( x, varName, onlyOwnLevels = TRUE ) {
 
    xDat <- x$eval
    yDat <- fitted(x) + residuals(x)
-   grad <- gradients( x )
-   if( is.null( grad ) ) {
-      stop( "the model specified by argument 'x' does not include gradients" )
-   }
+
    if( ! varName %in% x$xnames ) {
       stop( "the model specified by argument 'x'",
          " does not include an explanatory variable '", varName, "'" )
@@ -17,17 +14,13 @@ npregGradFactor <- function( x, varName, onlyOwnLevels = TRUE ) {
       stop( "internal error: the data set in x$eval",
          " does not include a variable '", varName, "'" )
    }
-   if( nrow( grad ) != length( xDat[[ varName ]] ) ) {
-      stop( "internal error: gradients have a different number of observations",
-         " than variable '", varName, "'" )
-   }
-   colnames( grad ) <- x$xnames
+
    allLevels <- levels( xDat[[ varName ]] )
    nLevels <- length( allLevels )
    if( nLevels < 2 ) {
       stop( "the factor variable '", varName, "' has must have at least two levels" )
    }
-   gradFac <- matrix( NA, nrow = nrow( grad ), ncol = nLevels - 1 )
+   gradFac <- matrix( NA, nrow = nrow( xDat ), ncol = nLevels - 1 )
    if( is.ordered( xDat[[ varName ]] ) ) {
       colnames( gradFac ) <- paste( varName, ": ", 
          allLevels[ - length( allLevels ) ], " -> ", allLevels[-1], sep = "" )
@@ -36,19 +29,18 @@ npregGradFactor <- function( x, varName, onlyOwnLevels = TRUE ) {
          allLevels[-1], sep = "" )
    }
    for( i in 2:nLevels ) {
+      xDatBase <- xDatNew <- xDat
+      xDatBase[[ varName ]][] <- levels( xDat[[ varName ]] )[
+         ifelse( is.ordered( xDat[[ varName ]] ), i - 1, 1 ) ]
+      xDatNew[[ varName ]][] <- levels( xDat[[ varName ]] )[i]
+      modelBase <- npreg( bws = x$bws, tydat = yDat, txdat = xDat, 
+         exdat = xDatBase )
+      modelNew <- npreg( bws = x$bws, tydat = yDat, txdat = xDat,
+         exdat = xDatNew )
+      gradFac[ , i - 1 ] <- fitted( modelNew ) - fitted( modelBase )         
       if( onlyOwnLevels ) {
-         obsSelect <-  xDat[[ varName ]] == allLevels[ i ]
-         gradFac[ obsSelect, i - 1 ] <- grad[ obsSelect, varName ]
-      } else {
-         xDatBase <- xDatNew <- xDat
-         xDatBase[[ varName ]][] <- levels( xDat[[ varName ]] )[
-            ifelse( is.ordered( xDat[[ varName ]] ), i - 1, 1 ) ]
-         xDatNew[[ varName ]][] <- levels( xDat[[ varName ]] )[i]
-         modelBase <- npreg( bws = x$bws, tydat = yDat, txdat = xDat, 
-            exdat = xDatBase )
-         modelNew <- npreg( bws = x$bws, tydat = yDat, txdat = xDat,
-            exdat = xDatNew )
-         gradFac[ , i - 1 ] <- fitted( modelNew ) - fitted( modelBase )         
+         obsOwnLevel <-  xDat[[ varName ]] == allLevels[ i ]
+         gradFac[ !obsOwnLevel, i - 1 ] <- NA
       }
    }
    return( gradFac )
