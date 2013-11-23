7627eff7f056d1f5cc9334ca04c75f4ddd0bd2af
arne (Arne Henningsen) <arne.henningsen@gmail.com>
2013-11-23 19:28:02 +0000

    added function npregCv() for leave-one-out cross-validation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@1913 95fa4d17-b1e8-0310-9d42-a43d67248e42

---
+++ pkg/micEconNP/R/npregCv.R
@@ -0,0 +1,14 @@
+npregCv <- function( x ) {
+   
+   xDat <- x$eval
+   yDat <- fitted(x) + residuals(x)
+   
+   res <- rep( NA, nrow( xDat ) )
+   for( i in 1:nrow( xDat ) ) {
+      tmp <- npreg( bws = x$bws, tydat = yDat[-i], txdat = xDat[-i,], 
+         exdat = xDat[i,] )
+      res[ i ] <- yDat[ i ] - fitted( tmp )
+   }
+   cv <- mean( res^2 )
+   return( cv )
+}
