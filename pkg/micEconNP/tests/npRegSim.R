b001afdc5de7765bcd7a3b34d238f3af34ea944d
arne (Arne Henningsen) <arne.henningsen@gmail.com>
2013-11-23 19:39:36 +0000

    added tests of npregCv()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@1914 95fa4d17-b1e8-0310-9d42-a43d67248e42

---
+++ pkg/micEconNP/tests/npRegSim.R
@@ -0,0 +1,13 @@
+library( "micEconNP" )
+options( np.messages = FALSE )
+set.seed(42)
+n <- 250
+x1 <- rnorm(n)
+x2 <- rnorm(n)
+y <- 1 + x1 + x2^2 + rnorm(n)
+
+model <- npreg( y ~ x1 + x2, regtype="ll" )
+
+cv <- npregCv( model )
+round( cv, 3 )
+all.equal( cv, model$bws$fval )
