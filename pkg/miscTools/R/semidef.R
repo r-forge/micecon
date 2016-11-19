9548b4296ccb0eb26ba3a4f44a3385cf5138cd07
arne (Arne Henningsen) <arne.henningsen@gmail.com>
2016-11-19 20:19:56 +0000

    simplified code in semidefiniteness(): if argument 'positive' is FALSE, -m is tested instead of m
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@2012 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- pkg/miscTools/R/semidef.R
+++ pkg/miscTools/R/semidef.R
@@ -16,27 +16,18 @@ semidefiniteness <- function( m, positive = TRUE, tol = .Machine$double.eps,
          stop( "argument 'm' or each of its elements must be a _quadratic_ matrix" )
       }
       n <- nrow( m )
+      if( !positive ) {
+         m <- -m
+      }
       if( method == "det" ) {
-         if( positive ) {
-            result <- ( min( diag( m ) ) >= -tol )
-         } else {
-            result <- ( max( diag( m ) ) <= tol )
-         }
+         result <- ( min( diag( m ) ) >= -tol )
          if( n > 1 ) {
             for( i in 2:n ) {
-               if( positive ) {
-                  result <- result && ( det( m[ 1:i, 1:i ] ) >= -tol )
-               } else {
-                  result <- result && ( det( m[ 1:i, 1:i ] ) * ( -1 )^i >= -tol )
-               }
+               result <- result && ( det( m[ 1:i, 1:i ] ) >= -tol )
             }
          }
       } else if( method == "eigen" ) {
-         if( positive ) {
-            result <- ( min( eigen( m )$values ) > -tol )
-         } else {
-            result <- ( max( eigen( m )$values ) < tol )
-         }
+         result <- ( min( eigen( m )$values ) > -tol )
       } else {
          stop( "argument 'method' must be either 'det' or 'eigen'" )
       }
