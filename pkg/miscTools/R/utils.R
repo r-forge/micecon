b4a177d9fb3f4d7d4eaa598f7a1f2d14c7ff4e61
arne <arne@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2016-11-19 15:01:02 +0000

    moved code for defining function semidefiniteness() into a separate file semidef.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@2010 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- pkg/miscTools/R/utils.R
+++ pkg/miscTools/R/utils.R
@@ -186,51 +186,6 @@ rSquared <- function( y, resid ) {
    return( r2 )
 }
 
-## ----- test positive / negative semidefiniteness
-semidefiniteness <- function( m, positive = TRUE, tol = .Machine$double.eps,
-      method = "det" ) {
-
-   result <- list ()
-   if( is.list( m ) ) {
-      result <- logical( length( m ) )
-      for( t in 1:length( m ) ) {
-         result[ t ] <- semidefiniteness( m[[ t ]], positive = positive,
-            tol = tol, method = method )
-      }
-   } else if( !is.matrix( m ) ) {
-      stop( "argument 'm' must be a matrix or a list of matrices" )
-   } else {
-      if( nrow( m ) != ncol( m ) ) {
-         stop( "argument 'm' or each of its elements must be a _quadratic_ matrix" )
-      }
-      n <- nrow( m )
-      if( method == "det" ) {
-         if( positive ) {
-            result <- ( min( diag( m ) ) >= -tol )
-         } else {
-            result <- ( max( diag( m ) ) <= tol )
-         }
-         if( n > 1 ) {
-            for( i in 2:n ) {
-               if( positive ) {
-                  result <- result && ( det( m[ 1:i, 1:i ] ) >= -tol )
-               } else {
-                  result <- result && ( det( m[ 1:i, 1:i ] ) * ( -1 )^i >= -tol )
-               }
-            }
-         }
-      } else if( method == "eigen" ) {
-         if( positive ) {
-            result <- ( min( eigen( m )$values ) > -tol )
-         } else {
-            result <- ( max( eigen( m )$values ) < tol )
-         }
-      } else {
-         stop( "argument 'method' must be either 'det' or 'eigen'" )
-      }
-   }
-   return( result )
-}
 
 ## --- creates a symmetric matrix ----
 symMatrix <- function( data = NA, nrow = NULL, byrow = FALSE,
