13f64bde3c7584688aa2b6288e777859a2478015
arne (Arne Henningsen) <arne.henningsen@gmail.com>
2019-10-11 06:47:50 +0000

    added round() commands to make the tests of stdEr() more robust to rounding errors
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@2114 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- pkg/miscTools/tests/stdErTests.R
+++ pkg/miscTools/tests/stdErTests.R
@@ -1,14 +1,14 @@
 ## testing stdEr() methods
-library( miscTools )
-data(cars)
+library( "miscTools" )
+data( "cars" )
 
 # lm()
 lmRes <- lm(dist ~ speed, data=cars)
 summary( lmRes )
-stdEr( lmRes )
+round( stdEr( lmRes ), 4 )
 
 # nls()
 nlsRes <- nls( dist ~ b0 + b1 * speed^b2, start = c( b0=0, b1=1, b2=1 ),
    data = cars )
 summary( nlsRes )
-stdEr( nlsRes )
+round( stdEr( nlsRes ), 3 )
