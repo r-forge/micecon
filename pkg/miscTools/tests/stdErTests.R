d614699486edb302b12b42b8f9f3121e45d48a7c
arne <arne@95fa4d17-b1e8-0310-9d42-a43d67248e42>
2019-10-11 07:22:16 +0000

    made the tests of stdEr() more robust to rounding errors
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/micecon@2115 95fa4d17-b1e8-0310-9d42-a43d67248e42

--- pkg/miscTools/tests/stdErTests.R
+++ pkg/miscTools/tests/stdErTests.R
@@ -10,5 +10,7 @@ round( stdEr( lmRes ), 4 )
 # nls()
 nlsRes <- nls( dist ~ b0 + b1 * speed^b2, start = c( b0=0, b1=1, b2=1 ),
    data = cars )
-summary( nlsRes )
+tmp <- summary( nlsRes )
+tmp$convInfo$finTol <- round( tmp$convInfo$finTol, 4 )
+print( tmp )
 round( stdEr( nlsRes ), 3 )
